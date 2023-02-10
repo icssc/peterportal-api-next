7367842bfe886f12a71c3b3fe458332998e40060
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-02-09 19:49:28 -0800

    feat(ddb): implement createTable and describeTable wrappers

--- ddb/index.ts
+++ ddb/index.ts
@@ -1,4 +1,15 @@
-import { DynamoDB, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import {
+  BillingMode,
+  CreateTableCommand,
+  DescribeTableCommand,
+  DescribeTableOutput,
+  DynamoDB,
+  DynamoDBClientConfig,
+  KeyType,
+  ScalarAttributeType,
+  TableClass,
+  TableStatus,
+} from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
   GetCommand,
@@ -8,8 +19,16 @@ import {
   TranslateConfig,
 } from "@aws-sdk/lib-dynamodb";
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export type Key = {
+  name: string;
+  type: ScalarAttributeType;
+};
+
 export class DDBDocClient {
-  private client: DynamoDBDocumentClient;
+  private readonly client: DynamoDB;
+  private readonly documentClient: DynamoDBDocumentClient;
   constructor(
     configuration: DynamoDBClientConfig = {
       /* eslint-disable turbo/no-undeclared-env-vars */
@@ -31,27 +50,89 @@ export class DDBDocClient {
       },
     }
   ) {
-    this.client = DynamoDBDocumentClient.from(
-      new DynamoDB(configuration),
+    this.client = new DynamoDB(configuration);
+    this.documentClient = DynamoDBDocumentClient.from(
+      this.client,
       translateConfig
     );
   }
-  public get(
+  public async createTable(
+    tableName: string,
+    partitionKey: Key,
+    sortKey?: Key,
+    billingMode = BillingMode.PAY_PER_REQUEST,
+    tableClass = TableClass.STANDARD,
+    blocking = true
+  ): Promise<void> {
+    const attrDefs = [
+      {
+        AttributeName: partitionKey.name,
+        AttributeType: partitionKey.type,
+      },
+    ];
+    const keySchema = [
+      {
+        AttributeName: partitionKey.name,
+        KeyType: KeyType.HASH,
+      },
+    ];
+    if (sortKey) {
+      attrDefs.push({
+        AttributeName: sortKey.name,
+        AttributeType: sortKey.type,
+      });
+      keySchema.push({
+        AttributeName: sortKey.name,
+        KeyType: KeyType.RANGE,
+      });
+    }
+    await this.client.send(
+      new CreateTableCommand({
+        AttributeDefinitions: attrDefs,
+        KeySchema: keySchema,
+        TableName: tableName,
+        BillingMode: billingMode,
+        TableClass: tableClass,
+      })
+    );
+    if (blocking) {
+      for (;;) {
+        try {
+          if (
+            (await this.describeTable(tableName)).Table?.TableStatus ===
+            TableStatus.ACTIVE
+          )
+            return;
+          throw new Error();
+        } catch {
+          await sleep(3000);
+        }
+      }
+    }
+  }
+  public async describeTable(tableName: string): Promise<DescribeTableOutput> {
+    return this.client.send(
+      new DescribeTableCommand({
+        TableName: tableName,
+      })
+    );
+  }
+  public async get(
     tableName: string,
     key?: Record<string, never>
   ): Promise<GetCommandOutput> {
-    return this.client.send(
+    return this.documentClient.send(
       new GetCommand({
         TableName: tableName,
         Key: key,
       })
     );
   }
-  public put(
+  public async put(
     tableName: string,
     item: Record<string, never>
   ): Promise<PutCommandOutput> {
-    return this.client.send(
+    return this.documentClient.send(
       new PutCommand({ TableName: tableName, Item: item })
     );
   }
