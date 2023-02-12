2ee109bfeb6a93af56ccd9142dd699e16b3ea00b
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-02-11 19:04:48 -0800

    feat(websoc-scraper): set up webpack

---
+++ tools/websoc-scraper/webpack.config.cjs
@@ -0,0 +1,32 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  target: "node",
+  node: false,
+  entry: {
+    main: "./index.ts",
+    parent: "./parent/index.ts",
+    child: "./child/index.ts",
+  },
+  output: {
+    path: __dirname + "/dist/",
+    filename: "[name].js",
+    library: {
+      type: "commonjs2",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
