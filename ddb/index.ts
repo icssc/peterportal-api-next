80871ca52f2b72f05dfbfafb0a20db7159eb5996
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-02-21 10:19:03 -0800

    feat(ddb): deprecate createTable and describeTable

--- ddb/index.ts
+++ ddb/index.ts
@@ -1,15 +1,4 @@
-import {
-  BillingMode,
-  CreateTableCommand,
-  DescribeTableCommand,
-  DescribeTableOutput,
-  DynamoDB,
-  DynamoDBClientConfig,
-  KeyType,
-  ScalarAttributeType,
-  TableClass,
-  TableStatus,
-} from "@aws-sdk/client-dynamodb";
+import { DynamoDB, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
   GetCommand,
@@ -18,13 +7,6 @@ import {
   PutCommandOutput,
   TranslateConfig,
 } from "@aws-sdk/lib-dynamodb";
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-export type Key = {
-  name: string;
-  type: ScalarAttributeType;
-};
 
 export class DDBDocClient {
   private readonly client: DynamoDB;
@@ -48,67 +30,6 @@ export class DDBDocClient {
     this.documentClient = DynamoDBDocumentClient.from(
       this.client,
       translateConfig
-    );
-  }
-  public async createTable(
-    tableName: string,
-    partitionKey: Key,
-    sortKey?: Key,
-    billingMode = BillingMode.PAY_PER_REQUEST,
-    tableClass = TableClass.STANDARD,
-    blocking = true
-  ): Promise<void> {
-    const attrDefs = [
-      {
-        AttributeName: partitionKey.name,
-        AttributeType: partitionKey.type,
-      },
-    ];
-    const keySchema = [
-      {
-        AttributeName: partitionKey.name,
-        KeyType: KeyType.HASH,
-      },
-    ];
-    if (sortKey) {
-      attrDefs.push({
-        AttributeName: sortKey.name,
-        AttributeType: sortKey.type,
-      });
-      keySchema.push({
-        AttributeName: sortKey.name,
-        KeyType: KeyType.RANGE,
-      });
-    }
-    await this.client.send(
-      new CreateTableCommand({
-        AttributeDefinitions: attrDefs,
-        KeySchema: keySchema,
-        TableName: tableName,
-        BillingMode: billingMode,
-        TableClass: tableClass,
-      })
-    );
-    if (blocking) {
-      for (;;) {
-        try {
-          if (
-            (await this.describeTable(tableName)).Table?.TableStatus ===
-            TableStatus.ACTIVE
-          )
-            return;
-          throw new Error();
-        } catch {
-          await sleep(3000);
-        }
-      }
-    }
-  }
-  public async describeTable(tableName: string): Promise<DescribeTableOutput> {
-    return this.client.send(
-      new DescribeTableCommand({
-        TableName: tableName,
-      })
     );
   }
   public async get(
