5a229bdc2e7167109c632b2cda20ad42fef1c5bf
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-02-24 21:44:50 -0800

    feat(websoc-api-next): go back to using camaro

--- api/routes/websoc/build.mjs
+++ api/routes/websoc/build.mjs
@@ -1,5 +1,5 @@
 import { build } from "esbuild";
-import { mkdir, rm } from "fs/promises";
+import { cp, mkdir, rm } from "fs/promises";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
@@ -9,6 +9,7 @@ import { fileURLToPath } from "url";
   const options = {
     bundle: true,
     entryPoints: [join(cwd, "index.ts")],
+    external: ["camaro"],
     logLevel: "info",
     minify: true,
     outfile: join(cwd, "dist/index.cjs"),
@@ -20,6 +21,35 @@ import { fileURLToPath } from "url";
           build.onStart(async () => {
             await rm(join(cwd, "dist/"), { recursive: true, force: true });
             await mkdir(join(cwd, "dist/"));
+            await mkdir(join(cwd, "dist/node_modules"));
+          });
+        },
+      },
+      {
+        name: "copy",
+        setup(build) {
+          build.onEnd(async () => {
+            await Promise.all(
+              [
+                "@assemblyscript/loader",
+                "base64-js",
+                "camaro",
+                "eventemitter-asyncresource",
+                "hdr-histogram-js",
+                "hdr-histogram-percentiles-obj",
+                "nice-napi",
+                "node-addon-api",
+                "node-gyp-build",
+                "pako",
+                "piscina",
+              ].map((module) =>
+                cp(
+                  join(cwd, `../../../node_modules/${module}`),
+                  join(cwd, `dist/node_modules/${module}`),
+                  { recursive: true }
+                )
+              )
+            );
           });
         },
       },
