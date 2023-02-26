e16136238f73cf2ed286df71a561ec11b2c38ac2
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-02-26 00:45:21 -0800

    fix: remove generics from restResolverFactory

--- api/routes/graphql/lib.ts
+++ api/routes/graphql/lib.ts
@@ -9,15 +9,15 @@ import { encode } from "querystring";
  * Instantiates and returns a resolver that queries the specified REST endpoint.
  * @param path The path to the REST endpoint.
  */
-export const restResolverFactory = <TArgs, TRes>(path: string) => {
-  return async (_: never, args: TArgs): Promise<TRes> => {
-    const res: RawResponse<TRes> = await (
+export const restResolverFactory = (path: string) => {
+  return async (_: never, args: ParsedUrlQueryInput): Promise<unknown> => {
+    const res: RawResponse<unknown> = await (
       await fetch(
         `${
           process.env.NODE_ENV === "development"
             ? `http://localhost:${process.env.PORT || 8080}`
             : "https://api-next.peterportal.org"
-        }${path}?${encode(args as ParsedUrlQueryInput)}`
+        }${path}?${encode(args)}`
       )
     ).json();
     if (isErrorResponse(res))
--- api/routes/graphql/resolver/websoc.ts
+++ api/routes/graphql/resolver/websoc.ts
@@ -1,12 +1,7 @@
-import type { Term, WebsocAPIResponse } from "peterportal-api-next-types";
-import type { WebsocAPIOptions } from "websoc-api-next";
-
 import { restResolverFactory } from "../lib";
 
 export default {
   Query: {
-    websoc: restResolverFactory<Term & WebsocAPIOptions, WebsocAPIResponse>(
-      "/v1/rest/websoc"
-    ),
+    websoc: restResolverFactory("/v1/rest/websoc"),
   },
 };
