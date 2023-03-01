3e17df0f6e8f7dc507b7ab2afa511f54823db00f
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-01 15:44:58 -0800

    docs: improve restResolverFactory docstring

--- api/routes/graphql/lib.ts
+++ api/routes/graphql/lib.ts
@@ -6,17 +6,26 @@ import type { ParsedUrlQueryInput } from "querystring";
 import { encode } from "querystring";
 
 /**
- * Instantiates and returns a resolver that, when called, queries the specified
- * REST endpoint and returns the result.
+ * Returns a resolver function for a specific GraphQL field that queries the
+ * specified REST endpoint.
+ *
+ * The resolver function takes in the parent argument (which is unused) and an
+ * object that contains all GraphQL arguments provided for this field. It then
+ * constructs a GET request to send to the given REST endpoint. If the response
+ * is an error, then it throws a ``GraphQLError``, which causes the server to
+ * return an error response compliant with the GraphQL specification. Otherwise,
+ * it returns the payload from the response, which the server returns as data.
  * @param path The path to the REST endpoint.
  * @param transform A function that transforms the arguments passed to the
  * resolver. Defaults to the identity function, if no transformation is needed.
  */
-export const restResolverFactory = (
-  path: string,
-  transform: (args: ParsedUrlQueryInput) => ParsedUrlQueryInput = (args) => args
-) => {
-  return async (_: never, args: ParsedUrlQueryInput): Promise<unknown> => {
+export const restResolverFactory =
+  (
+    path: string,
+    transform: (args: ParsedUrlQueryInput) => ParsedUrlQueryInput = (args) =>
+      args
+  ) =>
+  async (_: never, args: ParsedUrlQueryInput): Promise<unknown> => {
     const res: RawResponse<unknown> = await (
       await fetch(
         `${
@@ -34,4 +43,3 @@ export const restResolverFactory = (
       });
     return res.payload;
   };
-};
