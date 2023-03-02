4e269fa53242f77b545af7ef849f22f18049458f
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-02 10:55:57 -0800

    fix(docs): make layout less bad

--- docs/src/pages/apolloSandbox.tsx
+++ docs/src/pages/apolloSandbox.tsx
@@ -1,5 +1,6 @@
 import { ApolloSandbox as AS } from "@apollo/sandbox/react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
+import Layout from "@theme/Layout";
 import React from "react";
 
 import styles from "./apolloSandbox.module.css";
@@ -8,16 +9,18 @@ export default function ApolloSandbox() {
   return (
     <BrowserOnly fallback={<>"Loading..."</>}>
       {() => (
-        <AS
-          className={styles.apolloSandbox}
-          initialEndpoint={`${
-            process.env.NODE_ENV === "development"
-              ? `http://localhost:${process.env.PORT || 8080}`
-              : `https://${
-                  process.env.STAGE === "prod" ? "" : `${process.env.STAGE}-`
-                }api-next.peterportal.org`
-          }/v1/graphql`}
-        />
+        <Layout title={"Sandbox"} noFooter>
+          <AS
+            className={styles.apolloSandbox}
+            initialEndpoint={`${
+              process.env.NODE_ENV === "development"
+                ? `http://localhost:${process.env.PORT || 8080}`
+                : `https://${
+                    process.env.STAGE === "prod" ? "" : `${process.env.STAGE}-`
+                  }api-next.peterportal.org`
+            }/v1/graphql`}
+          />
+        </Layout>
       )}
     </BrowserOnly>
   );
