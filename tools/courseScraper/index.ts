bca8300d9ed81c12ce1bb5531d3a0f8bb1a68f29
Dg155 <gosianinc@gmail.com>
2023-03-01 21:10:29 -0800

    feat(tools/coursescraper): finished up the getAllCourses and parseCourseBody functions

--- tools/courseScraper/index.ts
+++ tools/courseScraper/index.ts
@@ -8,11 +8,31 @@ const CATALOGUE_BASE_URL : string = "http://catalogue.uci.edu"
 const URL_TO_ALL_COURSES : string = CATALOGUE_BASE_URL + "/allcourses/"
 const URL_TO_ALL_SCHOOLS : string = CATALOGUE_BASE_URL + "/schoolsandprograms/"
 
+// references
+let GE_DICTIONARY: { [key:string] : string} = {
+    "Ia": "GE Ia: Lower Division Writing",
+    "Ib": "GE Ib: Upper Division Writing",
+    "II": "GE II: Science and Technology",
+    "III": "GE III: Social & Behavioral Sciences",
+    "IV": "GE IV: Arts and Humanities",
+    "Va": "GE Va: Quantitative Literacy",
+    "Vb": "GE Vb: Formal Reasoning",
+    "VI": "GE VI: Language Other Than English",
+    "VII": "GE VII: Multicultural Studies",
+    "VIII": "GE VIII: International/Global Issues"
+}
+
+
+/**
+ * @param ms milliseconds to sleep for
+ */
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
 /**
 * @param {string} s: string to normalize (usually parsed from cheerio object)
 * @returns {string}: a normalized string that can be safely compared to other strings
 */
-function normalizeString(s: string): string{
+export function normalizeString(s: string): string{
     return s.normalize("NFKD");
 }
 
@@ -20,7 +40,7 @@ function normalizeString(s: string): string{
  * @returns {Promise<{ [key: string]: string }>}: a mapping from department code to school name. Uses the catalogue.
  * Example: {"I&C SCI":"Donald Bren School of Information and Computer Sciences","IN4MATX":"Donald Bren School of Information and Computer Sciences"}
 */
-async function getDepartmentToSchoolMapping(): Promise<{ [key: string]: string }> {
+export async function getDepartmentToSchoolMapping(): Promise<{ [key: string]: string }> {
 
     /** 
      * helper function that takes a URL to a department page and maps the course page to each school on that page
@@ -71,20 +91,22 @@ async function getDepartmentToSchoolMapping(): Promise<{ [key: string]: string }
 
     console.log("Mapping Departments to Schools...");
     // some need to be hard coded (These are mentioned in All Courses but not listed in their respective school catalogue)
-    var mapping: { [key: string]: string } = {"FIN": "The Paul Merage School of Business",
-               "ARMN": "School of Humanities",
-               "BSEMD": "School of Biological Sciences",
-               "ECPS": "The Henry Samueli School of Engineering",
-               "BANA": "The Paul Merage School of Business",
-               "SPPS": "School of Social Sciences",
-               "UCDC": "Unaffiliated",
-               "ROTC": "Unaffiliated",
-               "NET SYS": "Donald Bren School of Information and Computer Sciences",
-               "UNI STU": "Unaffiliated",
-               "UNI AFF": "Unaffiliated",
-               "GDIM": "Donald Bren School of Information and Computer Sciences",
-               "DATA": "Donald Bren School of Information and Computer Sciences",
-               "EHS": "Unaffiliated"}
+    var mapping: { [key: string]: string } = {
+        "FIN": "The Paul Merage School of Business",
+        "ARMN": "School of Humanities",
+        "BSEMD": "School of Biological Sciences",
+        "ECPS": "The Henry Samueli School of Engineering",
+        "BANA": "The Paul Merage School of Business",
+        "SPPS": "School of Social Sciences",
+        "UCDC": "Unaffiliated",
+        "ROTC": "Unaffiliated",
+        "NET SYS": "Donald Bren School of Information and Computer Sciences",
+        "UNI STU": "Unaffiliated",
+        "UNI AFF": "Unaffiliated",
+        "GDIM": "Donald Bren School of Information and Computer Sciences",
+        "DATA": "Donald Bren School of Information and Computer Sciences",
+        "EHS": "Unaffiliated"
+    }
     const response = await axios.get(URL_TO_ALL_SCHOOLS);
     const $ = cheerio.load(response.data);           
     const schoolLinks: string[] = [];
@@ -106,7 +128,7 @@ async function getDepartmentToSchoolMapping(): Promise<{ [key: string]: string }
  * @param {string} courseURL: URL to a Courses page
  * @returns {void}: nothing, mutates the mapping passed in
 */
-async function mapCoursePageToSchool(mapping: { [key: string]: string }, school: string, courseURL: string) {
+export async function mapCoursePageToSchool(mapping: { [key: string]: string }, school: string, courseURL: string) {
     const response = await axios.get(courseURL);
     const $ = cheerio.load(response.data);
     // get all the departments under this school
@@ -137,7 +159,7 @@ async function mapCoursePageToSchool(mapping: { [key: string]: string }, school:
  * @returns {Promise<string[]>}: a list of class URLS from AllCourses
  * Example: ["http://catalogue.uci.edu/allcourses/ac_eng/","http://catalogue.uci.edu/allcourses/afam/",...]
 */
-async function getAllCourseURLS():Promise<string[]> {
+export async function getAllCourseURLS():Promise<string[]> {
     console.log("Collecting Course URLs from {" + URL_TO_ALL_COURSES + "}...");
     // store all URLS in list
     var courseURLS : string[] = [];
@@ -162,8 +184,9 @@ async function getAllCourseURLS():Promise<string[]> {
  * @param {{ [key: string]: string }} departmentToSchoolMapping: maps department code to its school {I&C SCI: Donald Bren School of Information and Computer Sciences}
  * @returns {void}: nothing, mutates the json_data passed in
 */
-async function getAllCourses(courseURL: string, json_data: { [key: string]: Object }, departmentToSchoolMapping: { [key: string]: string }){
+export async function getAllCourses(courseURL: string, json_data: { [key: string]: Object }, departmentToSchoolMapping: { [key: string]: string }){
     const response = await axios.get(courseURL);
+
     const $ = cheerio.load(response.data);
     // department name
     var department: string = normalizeString($("#contentarea > h1").text());
@@ -177,33 +200,70 @@ async function getAllCourses(courseURL: string, json_data: { [key: string]: Obje
             // course identification
             //courseBlocks.push(courseBlock);
             var courseInfo;
-            await getCourseInfo(courseBlock, courseURL).then((response) => {courseInfo = response;});
-            if (courseInfo != null){
-                const courseID: string = courseInfo[0];
-                const courseName: string = courseInfo[1];
-                const courseUnits: string = courseInfo[2];
-                // get course body (0:Course Description, 1:Prerequisite)
-                const courseBody = $(courseBlock).find('div > p');
-                const courseDescription: string = normalizeString($(courseBody[0]).text());
-                // parse units
-                let unit_range: string[];
-                if (courseUnits.includes("-")) {
-                    unit_range = courseUnits.split(" ")[0].split("-");
-                }
-                else {
-                    unit_range = [courseUnits.split(" ")[0], courseUnits.split(" ")[0]];
-                }
-                // parse course number and department
-                const splitID: string[] = courseID.split(" ");
-                const id_department: string = splitID.slice(0, -1).join(" ");
-                const id_number: string = splitID[splitID.length - 1];
-                console.log(splitID, id_department, id_number);
-                // error detection
-                if (!(id_department in departmentToSchoolMapping)) {
-                    noSchoolDepartment.add(id_department);
+            // wrap in try catch, and if fails sleep for a second and try again
+            while(courseInfo == null){
+                try {  
+                    await getCourseInfo(courseBlock, courseURL).then((response) => {courseInfo = response;});
+                } catch (error) {  
+                    await sleep(1000);
                 }
             }
-            else {throw new Error("CourseInfo is null");}
+            var courseID: string = courseInfo[0];
+            const courseName: string = courseInfo[1];
+            const courseUnits: string = courseInfo[2];
+            // get course body (0:Course Description, 1:Prerequisite)
+            const courseBody = $(courseBlock).find('div > p');
+            const courseDescription: string = normalizeString($(courseBody[0]).text());
+            // parse units
+            let unit_range: string[];
+            if (courseUnits.includes("-")) {
+                unit_range = courseUnits.split(" ")[0].split("-");
+            }
+            else {
+                unit_range = [courseUnits.split(" ")[0], courseUnits.split(" ")[0]];
+            }
+            // parse course number and department
+            const splitID: string[] = courseID.split(" ");
+            const id_department: string = splitID.slice(0, -1).join(" ");
+            const id_number: string = splitID[splitID.length - 1];
+            // error detection
+            if (!(id_department in departmentToSchoolMapping)) {
+                noSchoolDepartment.add(id_department);
+            }
+            // Examples at https://github.com/icssc-projects/PeterPortal/wiki/Course-Search
+            // store class data into object
+            const classInforamtion: { [key: string]: any } = {
+                "id": courseID.replace(" ", ""),
+                "department": id_department,
+                "number": id_number,
+                "school": id_department in departmentToSchoolMapping ? departmentToSchoolMapping[id_department] : "",
+                "title": courseName,
+                "course_level": determineCourseLevel(courseID),
+                //"department_alias": ALIASES[id_department] if id_department in ALIASES else [],"department_alias": ALIASES[id_department] if id_department in ALIASES else [],
+                "units": unit_range.map(x => parseFloat(x)),
+                "description": courseDescription,
+                "department_name": department,
+                "professor_history": [], "prerequisite_tree": "", "prerequisite_list": [], "prerequisite_text": "", "prerequisite_for": [], "repeatability": "", "grading_option": "",
+                "concurrent": "", "same_as": "", "restriction": "", "overlap": "", "corequisite": "", "ge_list": [], "ge_text": "", "terms": []
+            }
+            // key with no spaces
+            courseID = courseID.replace(" ", "")
+            // stores dictionaries in json_data to add dependencies later
+            json_data[courseID] = classInforamtion;
+            // populates the dic with simple information
+            parseCourseBody(courseBody, response, classInforamtion);
+
+            // try to parse prerequisite
+            if (courseBody.length > 1){
+                // IMPLIMENT const node = parsePrerequisite(courseBody[1], classInforamtion);
+                // maps the course to its requirement Node
+                // json_data[courseID]["node"] = node Why is this commented out?
+            }
+            // doesn't have any prerequisites
+            else {
+                //if debug:
+                    //print("\t\tNOREQS")
+            }
             // get course body (0:Course Description, 1:Prerequisite)
             //const courseBody = $(courseBlock).find('div').children();
         })
@@ -246,11 +306,88 @@ export async function getCourseInfo(courseBlock: cheerio.Element, courseURL: str
     }
 }
 
+/**
+ * @param {string} id_number: the number part of a course id (122A)
+ * @returns {string}: one of the three strings: (Lower Division (1-99), Upper Division (100-199), Graduate/Professional Only (200+))
+ * Example: "I&C Sci 33" => "(Lower Division (1-99)", "COMPSCI 143A" => "Upper Division (100-199)", "CompSci 206" => "Graduate/Professional Only (200+)"
+ */
+export function determineCourseLevel(id_number: string) {
+    // extract only the number 122A => 122
+    const courseString: string = id_number.replace(/\D/g, "");
+    if (courseString === "") { // if courseString is empty, then id_number did not contain any numbers
+        console.log("COURSE LEVEL ERROR, NO ID IN STRING", id_number);
+        return "";
+    }
+    const courseNumber: number = Number(courseString);
+    if (courseNumber < 100) {
+        return "Lower Division (1-99)";
+    } else if (courseNumber < 200) {
+        return "Upper Division (100-199)";
+    } else if (courseNumber >= 200) {
+        return "Graduate/Professional Only (200+)";
+    } else {
+        console.log("COURSE LEVEL ERROR", courseNumber);
+        return "";
+    }
+}
+
+/**
+ * @param {cheerio.Element} courseBody: a collection of ptags within a courseblock
+ * @param {object} response: response object from axios request on courseURL
+ * @param {dict} classInfo: a map to store parsed information
+ * @returns {void}: nothing, mutates the mapping passed in
+*/
+export async function parseCourseBody(courseBody: object, response:any, classInfo: { [key: string]: any }) {
+    const $ = cheerio.load(response.data);
+    // iterate through each ptag for the course
+    $(courseBody).each((i, ptag) => {
+        let pTagText = normalizeString($(ptag).text().trim());
+        // if starts with ( and has I or V in it, probably a GE tag
+        if (pTagText.length > 0 && pTagText[0] === '(' && (pTagText.includes('I') || pTagText.includes('V'))) {
+            // try to parse GE types
+            const ges: RegExp = /(?<type>[IV]+)\.?(?<subtype>[abAB]?)/g;
+            // if (debug) {
+            //     console.log('\t\tGE:');
+            // }
+            let match: RegExpExecArray | null;
+            while ((match = ges.exec(pTagText)) !== null) {
+                // normalize IA and VA to Ia and Va
+                const extractedGE: string = match.groups!.type + match.groups!.subtype.toLowerCase();
+                // normalize in full string also
+                pTagText = pTagText.replace(match.groups!.type + match.groups!.subtype, extractedGE);
+                // add to ge_types
+                classInfo['ge_list'].push(GE_DICTIONARY[extractedGE]);
+                // if (debug) {
+                // console.log(`${GE_DICTIONARY[extractedGE]} `);
+                // }
+            }
+            // if (debug) {
+            //     console.log();
+            // }
+            // store the full string
+            classInfo['ge_text'] = pTagText;
+        }
+        // try to match keywords like "grading option", "repeatability"
+        for (const keyWord of Object.keys(classInfo)) {
+            const possibleMatch: RegExpExecArray | null = RegExp(`^${keyWord.replace('_', ' ')}s?\\s?(with\\s)?(:\\s)?(?<value>.*)`, 'i').exec(pTagText);
+            if (possibleMatch && classInfo[keyWord].length === 0) {
+                classInfo[keyWord] = possibleMatch.groups!.value;
+                break;
+            }
+        }
+    })
+}
+
+
+//function parseCourseBody(courseBody, dic: dict):
+
 // getDepartmentToSchoolMapping().then((response) => {
 //     console.log(response);
 // });
 
 const noSchoolDepartment = new Set<string>();
+
+//console.log(determineCourseLevel("I&C Sci 33"));
 
 getAllCourseURLS().then((response) => {
     response.map(async (url) => {
