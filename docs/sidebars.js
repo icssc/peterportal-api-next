73f9b4b27e71e86b7b2d6c9fd8b308ad7c7d7e60
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-03 11:20:37 -0800

    feat: add rest api docs boilerplate

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,21 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  docsSidebar: ["getting-started", "fair-use-policy"],
+  docsSidebar: [
+    { type: "doc", id: "getting-started", label: "Getting Started" },
+    { type: "doc", id: "fair-use-policy", label: "Fair Use Policy" },
+    {
+      type: "category",
+      label: "REST API",
+      items: [
+        {
+          type: "doc",
+          id: "rest-api/getting-started",
+          label: "Getting Started",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = sidebars;
