7cf6a04f6149d2cd53239ad106cac91a73000454
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-03 15:35:49 -0800

    feat(types): modularize and document types

---
+++ packages/peterportal-api-next-types/types/instructor.ts
@@ -0,0 +1,47 @@
+/**
+ * An object representing an instructor.
+ * The type of the payload returned on a successful response from querying
+ * ``/v1/rest/instructors/{ucinetid}``.
+ * @alpha
+ */
+export type Instructor = {
+  /**
+   * The instructor's UCINetID.
+   */
+  ucinetid: string;
+  /**
+   * The full name of the instructor.
+   */
+  instructorName: string;
+  /**
+   * The shortened name (or WebSoc name; e.g. ``SHINDLER, M.``) of the instructor.
+   */
+  shortenedName: string;
+  /**
+   * The instructor's title.
+   */
+  title: string;
+  /**
+   * The department to which the instructor belongs.
+   */
+  department: string;
+  /**
+   * The school(s) associated with the instructor.
+   */
+  schools: string[];
+  /**
+   * The department(s) related to the instructor.
+   */
+  relatedDepartments: string[];
+  /**
+   * Course(s) this instructor has taught in the past.
+   */
+  courseHistory: string[];
+};
+
+/**
+ * The type of the payload returned on a successful response from querying
+ * ``/v1/rest/instructors``.
+ * @alpha
+ */
+export type Instructors = Instructor[];
---
+++ packages/peterportal-api-next-types/types/websoc.ts
@@ -0,0 +1,237 @@
+import { Quarter } from "./constants";
+
+/**
+ * The meeting time for a section.
+ */
+export type WebsocSectionMeeting = {
+  /**
+   * What day(s) the section meets on (e.g. ``MWF``).
+   */
+  days: string;
+  /**
+   * What time the section meets at.
+   */
+  time: string;
+  /**
+   * The building(s) the section meets in.
+   */
+  bldg: string[];
+};
+
+/**
+ * The enrollment statistics for a section.
+ */
+export type WebsocSectionEnrollment = {
+  /**
+   * The total number of students enrolled in this section.
+   */
+  totalEnrolled: string;
+  /**
+   * The number of students enrolled in the section referred to by this section
+   * code, if the section is cross-listed.
+   */
+  sectionEnrolled: string;
+};
+
+/**
+ * A WebSoc section object.
+ */
+export type WebsocSection = {
+  /**
+   * The section code.
+   */
+  sectionCode: string;
+  /**
+   * The section type (e.g. ``Lec``, ``Dis``, ``Lab``, etc.)
+   */
+  sectionType: string;
+  /**
+   * The section number (e.g. ``A1``).
+   */
+  sectionNum: string;
+  /**
+   * The number of units afforded by taking this section.
+   */
+  units: string;
+  /**
+   * The name(s) of the instructor(s) teaching this section.
+   */
+  instructors: string[];
+  /**
+   * The meeting time(s) of this section.
+   */
+  meetings: WebsocSectionMeeting[];
+  /**
+   * The date and time of the final exam for this section.
+   */
+  finalExam: string;
+  /**
+   * The maximum capacity of this section.
+   */
+  maxCapacity: string;
+  /**
+   * The number of students currently enrolled (cross-listed or otherwise) in
+   * this section.
+   */
+  numCurrentlyEnrolled: WebsocSectionEnrollment;
+  /**
+   * The number of students currently on the waitlist for this section.
+   */
+  numOnWaitlist: string;
+  /**
+   * The number of students who have requested to be enrolled in this section.
+   */
+  numRequested: string;
+  /**
+   * The number of seats in this section reserved for new students.
+   */
+  numNewOnlyReserved: string;
+  /**
+   * The restriction code(s) for this section.
+   */
+  restrictions: string;
+  /**
+   * The enrollment status.
+   */
+  status: "OPEN" | "Waitl" | "FULL" | "NewOnly";
+  /**
+   * Any comments for the section.
+   */
+  sectionComment: string;
+};
+
+/**
+ * A WebSoc course object.
+ */
+export type WebsocCourse = {
+  /**
+   * The code of the department the course belongs to.
+   */
+  deptCode: string;
+  /**
+   * The course number.
+   */
+  courseNumber: string;
+  /**
+   * The title of the course.
+   */
+  courseTitle: string;
+  /**
+   * Any comments for the course.
+   */
+  courseComment: string;
+  /**
+   * The link to the WebReg Course Prerequisites page for this course.
+   */
+  prerequisiteLink: string;
+  /**
+   * All sections of the course.
+   */
+  sections: WebsocSection[];
+};
+
+/**
+ * A WebSoc department object.
+ */
+export type WebsocDepartment = {
+  /**
+   * The name of the department.
+   */
+  deptName: string;
+  /**
+   * The department code.
+   */
+  deptCode: string;
+  /**
+   * Any comments from the department.
+   */
+  deptComment: string;
+  /**
+   * All courses of the department.
+   */
+  courses: WebsocCourse[];
+  /**
+   * Any comments for section code(s) under the department.
+   */
+  sectionCodeRangeComments: string[];
+  /**
+   * Any comments for course number(s) under the department.
+   */
+  courseNumberRangeComments: string[];
+};
+
+/**
+ * A WebSoc school object.
+ */
+export type WebsocSchool = {
+  /**
+   * The name of the school.
+   */
+  schoolName: string;
+  /**
+   * Any comments from the school.
+   */
+  schoolComment: string;
+  /**
+   * All departments of the school.
+   */
+  departments: WebsocDepartment[];
+};
+
+/**
+ * An object that represents a specific term.
+ */
+export type Term = {
+  /**
+   * The year of the term.
+   */
+  year: string;
+  /**
+   * The quarter of the term.
+   */
+  quarter: Quarter;
+};
+
+/**
+ * The type of the payload returned on a successful response from querying
+ * ``/v1/rest/websoc``.
+ */
+export type WebsocAPIResponse = {
+  /**
+   * All schools matched by the query.
+   */
+  schools: WebsocSchool[];
+};
+
+/**
+ * The type of the payload returned on a successful response from querying
+ * ``/v1/rest/websoc/departments``.
+ */
+export type Department = {
+  /**
+   * A string containing the department code and name.
+   * @example ``COMPSCI: Computer Science``
+   */
+  deptLabel: string;
+  /**
+   * The department code.
+   */
+  deptValue: string;
+};
+
+/**
+ * The type of the payload returned on a successful response from querying
+ * ``/v1/rest/terms``.
+ */
+export type TermData = {
+  /**
+   * The short name of the term.
+   * @example ``2023 Spring``
+   */
+  shortName: `${string} ${Quarter}`;
+  /**
+   * The full name of the term.
+   * @example ``2023 Spring Quarter``
+   */
+  longName: string;
+};
