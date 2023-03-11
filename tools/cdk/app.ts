103060968a600a75812cdf08a9a963189c896120
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-10 19:17:51 -0800

    feat: nuke websoc cache updater

--- tools/cdk/app.ts
+++ tools/cdk/app.ts
@@ -3,15 +3,8 @@ import "dotenv/config";
 
 import { App, StackProps } from "aws-cdk-lib";
 
-import { WebsocCacheUpdaterStack } from "./websocCacheUpdaterStack.js";
-
 const app = new App({ autoSynth: true });
 const props: StackProps = {
   env: { region: "us-east-1" },
   terminationProtection: /* true */ false,
 };
-new WebsocCacheUpdaterStack(
-  app,
-  "peterportal-api-next-websoc-cache-updater-stack",
-  props
-);
