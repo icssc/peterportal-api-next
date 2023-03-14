fd10f5484fff742ba95ebde1769925b5ccab3a14
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-14 12:12:15 -0700

    feat: implement REST endpoint

---
+++ api/routes/grades/lib.ts
@@ -0,0 +1,105 @@
+import { Prisma } from "db";
+import type {
+  GradeDistribution,
+  GradesAggregate,
+  GradesRaw,
+} from "peterportal-api-next-types";
+
+import { Query } from "./schema";
+
+const isNotPNPOnly = ({
+  gradeACount,
+  gradeBCount,
+  gradeCCount,
+  gradeDCount,
+  gradeFCount,
+}: GradeDistribution) =>
+  gradeACount || gradeBCount || gradeCCount || gradeDCount || gradeFCount;
+
+export function constructPrismaQuery(
+  parsedQuery: Query
+): Prisma.GradesSectionWhereInput {
+  const {
+    year,
+    quarter,
+    instructor,
+    department,
+    courseNumber,
+    sectionCode,
+    division,
+    excludePNP,
+  } = parsedQuery;
+  const courseNumeric: Prisma.IntFilter = {};
+  switch (division) {
+    case "LowerDiv":
+      courseNumeric.gte = 0;
+      courseNumeric.lte = 99;
+      break;
+    case "UpperDiv":
+      courseNumeric.gte = 100;
+      courseNumeric.lte = 199;
+      break;
+    case "Graduate":
+      courseNumeric.gte = 200;
+      break;
+  }
+  const excludePNPFilters: Prisma.GradesSectionWhereInput = {};
+  if (excludePNP) {
+    excludePNPFilters.gradeACount = 0;
+    excludePNPFilters.gradeBCount = 0;
+    excludePNPFilters.gradeCCount = 0;
+    excludePNPFilters.gradeDCount = 0;
+    excludePNPFilters.gradeFCount = 0;
+  }
+  return {
+    year,
+    quarter,
+    instructors: { some: { name: { contains: instructor } } },
+    department,
+    courseNumber,
+    courseNumeric,
+    sectionCode,
+    ...excludePNPFilters,
+  };
+}
+
+export function aggregateGrades(grades: GradesRaw): GradesAggregate {
+  return {
+    sectionList: grades.map(
+      ({
+        year,
+        quarter,
+        department,
+        courseNumber,
+        sectionCode,
+        instructors,
+      }) => ({
+        year,
+        quarter,
+        department,
+        courseNumber,
+        sectionCode,
+        instructors,
+      })
+    ),
+    gradeDistribution: {
+      ...(Object.fromEntries(
+        (
+          [
+            "gradeACount",
+            "gradeBCount",
+            "gradeCCount",
+            "gradeDCount",
+            "gradeFCount",
+            "gradePCount",
+            "gradeNPCount",
+            "gradeWCount",
+          ] as (keyof GradeDistribution)[]
+        ).map((key) => [key, grades.reduce((a, { [key]: b }) => a + b, 0)])
+      ) as Omit<GradeDistribution, "averageGPA">),
+      averageGPA:
+        grades.reduce((a, { averageGPA: b }) => a + b, 0) /
+        grades.filter(isNotPNPOnly).length,
+    },
+  };
+}
---
+++ api/routes/grades/schema.ts
@@ -0,0 +1,23 @@
+import { divisionCodes, quarters } from "peterportal-api-next-types";
+import { z } from "zod";
+
+export const QuerySchema = z.object({
+  year: z
+    .string()
+    .regex(/^\d{4}$/, { message: "Invalid year provided" })
+    .optional(),
+  quarter: z
+    .enum(quarters, { invalid_type_error: "Invalid quarter provided" })
+    .optional(),
+  instructor: z.string().optional(),
+  department: z.string().optional(),
+  courseNumber: z.string().optional(),
+  sectionCode: z
+    .string()
+    .regex(/^\d{5}$/, { message: "Invalid sectionCode provided" })
+    .optional(),
+  division: z.enum(divisionCodes).optional(),
+  excludePNP: z.boolean().optional(),
+});
+
+export type Query = z.TypeOf<typeof QuerySchema>;
