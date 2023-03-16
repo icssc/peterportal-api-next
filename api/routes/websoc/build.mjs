48e7e6a77a331f179d4e34df3b80a3ccd4898b27
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-16 12:01:34 -0700

    build: refactor websoc route script

--- api/routes/websoc/build.mjs
+++ api/routes/websoc/build.mjs
@@ -3,10 +3,37 @@ import { chmod, copyFile, cp, mkdir, rm } from "fs/promises";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
-(async () => {
-  const cwd = dirname(fileURLToPath(import.meta.url));
-  /** @type {import("esbuild").BuildOptions} */
-  const options = {
+// ESM hack for __dirname
+const cwd = dirname(fileURLToPath(import.meta.url));
+
+// The array of packages that ``camaro`` depends on.
+// All of these need to be copied into ``dist/node_modules`` after build.
+const camaroDeps = [
+  "@assemblyscript/loader",
+  "base64-js",
+  "camaro",
+  "eventemitter-asyncresource",
+  "hdr-histogram-js",
+  "hdr-histogram-percentiles-obj",
+  "nice-napi",
+  "node-addon-api",
+  "node-gyp-build",
+  "pako",
+  "piscina",
+];
+
+// The relative path to the generated Prisma Client.
+const prismaClientDir = "../../../node_modules/.prisma/client/";
+
+/*
+ * The file name of the Prisma query engine. This needs to be copied into the
+ * same directory as the bundle.
+ * @see {@link https://www.prisma.io/docs/concepts/components/prisma-client/module-bundlers}
+ */
+const prismaQueryEngine = "libquery_engine-rhel-openssl-1.0.x.so.node";
+
+async function buildApp() {
+  await build({
     bundle: true,
     entryPoints: [join(cwd, "index.ts")],
     external: ["camaro"],
@@ -29,21 +56,8 @@ import { fileURLToPath } from "url";
         name: "copy",
         setup(build) {
           build.onEnd(async () => {
-            // camaro
             await Promise.all(
-              [
-                "@assemblyscript/loader",
-                "base64-js",
-                "camaro",
-                "eventemitter-asyncresource",
-                "hdr-histogram-js",
-                "hdr-histogram-percentiles-obj",
-                "nice-napi",
-                "node-addon-api",
-                "node-gyp-build",
-                "pako",
-                "piscina",
-              ].map((module) =>
+              camaroDeps.map((module) =>
                 cp(
                   join(cwd, `../../../node_modules/${module}`),
                   join(cwd, `dist/node_modules/${module}`),
@@ -53,25 +67,20 @@ import { fileURLToPath } from "url";
             );
             // prisma
             await copyFile(
-              join(
-                cwd,
-                "../../../node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node"
-              ),
-              join(cwd, "dist/libquery_engine-rhel-openssl-1.0.x.so.node")
+              join(cwd, `${prismaClientDir}${prismaQueryEngine}`),
+              join(cwd, `dist/${prismaQueryEngine}`)
             );
             await copyFile(
-              join(cwd, "../../../node_modules/.prisma/client/schema.prisma"),
+              join(cwd, `${prismaClientDir}schema.prisma`),
               join(cwd, "dist/schema.prisma")
             );
-            await chmod(
-              join(cwd, "dist/libquery_engine-rhel-openssl-1.0.x.so.node"),
-              0o755
-            );
+            await chmod(join(cwd, `dist/${prismaQueryEngine}`), 0o755);
           });
         },
       },
     ],
     target: "node16",
-  };
-  await build(options);
-})();
+  });
+}
+
+buildApp();
