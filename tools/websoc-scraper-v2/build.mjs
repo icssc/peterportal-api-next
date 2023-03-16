05ce73a7b252635416ced3b7ee9df575d119460b
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-16 12:16:54 -0700

    build: refactor scraper build script

--- tools/websoc-scraper-v2/build.mjs
+++ tools/websoc-scraper-v2/build.mjs
@@ -1,52 +1,45 @@
 import { copyFile, cp, mkdir, readdir, rm, writeFile } from "fs/promises";
 import { dirname, join } from "path";
-import { c } from "tar";
+import { create } from "tar";
 import { fileURLToPath } from "url";
 
 const cwd = dirname(fileURLToPath(import.meta.url));
 
+const targetDir = "dist/websoc-scraper-v2/";
+const sourceFiles = ["ecosystem.config.js", "index.ts", "package.json"];
+const dependencies = [".prisma", "db", "registrar-api", "websoc-api-next"];
+
 (async function main() {
   await rm(join(cwd, "dist/"), { recursive: true, force: true });
-  for (const dir of [
-    "dist/",
-    "dist/websoc-scraper-v2/",
-    "dist/websoc-scraper-v2/node_modules",
-  ]) {
-    await mkdir(join(cwd, dir));
-  }
+  await mkdir(join(cwd, `${targetDir}node_modules`), {
+    recursive: true,
+  });
   await Promise.all(
-    ["ecosystem.config.js", "index.ts", "package.json"].map((file) =>
-      copyFile(join(cwd, file), join(cwd, `dist/websoc-scraper-v2/${file}`))
+    sourceFiles.map((file) =>
+      copyFile(join(cwd, file), join(cwd, `${targetDir}${file}`))
     )
   );
   await Promise.all(
-    [".prisma", "db", "registrar-api", "websoc-api-next"].map((module) =>
+    dependencies.map((module) =>
       cp(
         join(cwd, `../../node_modules/${module}`),
-        join(cwd, `dist/websoc-scraper-v2/node_modules/${module}`),
+        join(cwd, `${targetDir}node_modules/${module}`),
         { dereference: true, recursive: true }
       )
     )
   );
   await Promise.all(
-    (
-      await readdir(
-        join(cwd, "dist/websoc-scraper-v2/node_modules/.prisma/client")
-      )
-    )
-      .filter((x) => x.includes(".node"))
-      .filter((x) => !x.includes("arm64"))
-      .map((x) =>
-        rm(join(cwd, "dist/websoc-scraper-v2/node_modules/.prisma/client", x))
-      )
+    (await readdir(join(cwd, `${targetDir}node_modules/.prisma/client`)))
+      .filter((x) => x.includes(".node") && !x.includes("arm64"))
+      .map((x) => rm(join(cwd, `${targetDir}node_modules/.prisma/client`, x)))
   );
   await writeFile(
-    join(cwd, "dist/websoc-scraper-v2/.env"),
+    join(cwd, "${targetDir}.env"),
     // eslint-disable-next-line turbo/no-undeclared-env-vars
     `DATABASE_URL="${process.env.DATABASE_URL_SCRAPER}"\nNODE_ENV="production"\nTZ="America/Los_Angeles"`
   );
   process.chdir(join(cwd, "dist"));
-  await c(
+  await create(
     {
       gzip: true,
       file: "websoc-scraper-v2.tar.gz",
