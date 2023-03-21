28c06520a1f2dec6824caf59c1ff70f20f628744
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-20 17:08:42 -0700

    build: change cwd to __dirname

--- tools/websoc-scraper-v2/build.mjs
+++ tools/websoc-scraper-v2/build.mjs
@@ -9,31 +9,33 @@ const sourceFiles = ["ecosystem.config.js", "index.ts", "package.json"];
 const dependencies = [".prisma", "db", "registrar-api", "websoc-api-next"];
 
 async function buildApp() {
-  await rm(join(cwd, targetDir), { recursive: true, force: true });
-  await mkdir(join(cwd, `${targetDir}node_modules`), {
+  await rm(join(__dirname, targetDir), { recursive: true, force: true });
+  await mkdir(join(__dirname, `${targetDir}node_modules`), {
     recursive: true,
   });
   await Promise.all(
     sourceFiles.map((file) =>
-      copyFile(join(cwd, file), join(cwd, `${targetDir}${file}`))
+      copyFile(join(__dirname, file), join(__dirname, `${targetDir}${file}`))
     )
   );
   await Promise.all(
     dependencies.map((module) =>
       cp(
-        join(cwd, `../../node_modules/${module}`),
-        join(cwd, `${targetDir}node_modules/${module}`),
+        join(__dirname, `../../node_modules/${module}`),
+        join(__dirname, `${targetDir}node_modules/${module}`),
         { dereference: true, recursive: true }
       )
     )
   );
   const prismaFiles = await readdir(
-    join(cwd, `${targetDir}node_modules/.prisma/client`)
+    join(__dirname, `${targetDir}node_modules/.prisma/client`)
   );
   await Promise.all(
     prismaFiles
       .filter((x) => x.includes(".node") && !x.includes("debian-openssl-1.1.x"))
-      .map((x) => rm(join(cwd, `${targetDir}node_modules/.prisma/client`, x)))
+      .map((x) =>
+        rm(join(__dirname, `${targetDir}node_modules/.prisma/client`, x))
+      )
   );
 }
 
