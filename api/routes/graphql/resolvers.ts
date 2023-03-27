1381bfad4d0b1f7e82ce629c76d35dee938d6b07
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-03-27 14:38:06 -0700

    feat: graphql support for grades options

--- api/routes/graphql/resolvers.ts
+++ api/routes/graphql/resolvers.ts
@@ -2,8 +2,9 @@ import { restResolverFactory } from "./lib";
 
 export default {
   Query: {
-    aggregateGrades: restResolverFactory("/v1/rest/grades/aggregate"),
     rawGrades: restResolverFactory("/v1/rest/grades/raw"),
+    aggregateGrades: restResolverFactory("/v1/rest/grades/aggregate"),
+    gradesOptions: restResolverFactory("/v1/rest/grades/options"),
     websoc: restResolverFactory("/v1/rest/websoc", (args) => ({
       ...args,
       ge: args.ge?.toString().replace("_", "-"),
