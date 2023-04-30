a70980c587ccc001542ed1926a80d660b4b3c809
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-04-29 18:06:23 -0700

    feat: enable lambda warming (#33)

--- api/routes/websoc/index.ts
+++ api/routes/websoc/index.ts
@@ -26,6 +26,14 @@ const prisma = new PrismaClient();
 
 export const rawHandler: RawHandler = async (request) => {
   const { method, path, query, requestId } = request.getParams();
+  if (request.isWarmerRequest()) {
+    try {
+      await prisma.$connect();
+      return createOKResult("Warmed", requestId);
+    } catch (e) {
+      createErrorResult(500, e, requestId);
+    }
+  }
   switch (method) {
     case "HEAD":
     case "GET":
