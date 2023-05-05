d23e2de1893e37e03c5057062205b041d049410e
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-05-04 18:15:52 -0700

    fix: add required_error

--- api/routes/calendar/schema.ts
+++ api/routes/calendar/schema.ts
@@ -2,8 +2,13 @@ import { quarters } from "peterportal-api-next-types";
 import { z } from "zod";
 
 export const QuerySchema = z.object({
-  year: z.string().regex(/^\d{4}$/, { message: "Invalid year provided" }),
-  quarter: z.enum(quarters, { invalid_type_error: "Invalid quarter provided" }),
+  year: z
+    .string({ required_error: 'Parameter "year" not provided' })
+    .regex(/^\d{4}$/, { message: "Invalid year provided" }),
+  quarter: z.enum(quarters, {
+    required_error: 'Parameter "quarter" not provided',
+    invalid_type_error: "Invalid quarter provided",
+  }),
 });
 
 export type Query = z.TypeOf<typeof QuerySchema>;
