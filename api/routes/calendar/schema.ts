a568f06e05b15bfe8c43ae041fe507c5eda5d913
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-05-10 14:51:55 -0700

    fix: use zod length instead of regex

--- api/routes/calendar/schema.ts
+++ api/routes/calendar/schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const QuerySchema = z.object({
   year: z
     .string({ required_error: 'Parameter "year" not provided' })
-    .regex(/^\d{4}$/, { message: "Invalid year provided" }),
+    .length(4, { message: "Invalid year provided" }),
   quarter: z.enum(quarters, {
     required_error: 'Parameter "quarter" not provided',
     invalid_type_error: "Invalid quarter provided",
