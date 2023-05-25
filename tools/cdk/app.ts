038a51178dd225e0a87b9a5e46d7a948b0150faa
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-05-25 13:25:57 -0700

    feat: add cdk code for proxy service

--- tools/cdk/app.ts
+++ tools/cdk/app.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import type { StackProps } from "aws-cdk-lib";
 import { App } from "aws-cdk-lib";
 
+import { WebsocProxyServiceStack } from "./websocProxyServiceStack";
 import { WebsocScraperV2Stack } from "./websocScraperV2Stack";
 
 const app = new App({ autoSynth: true });
@@ -11,4 +12,5 @@ const props: StackProps = {
   env: { region: "us-east-1" },
   terminationProtection: true,
 };
+new WebsocProxyServiceStack(app, "peterportal-api-next-websoc-proxy-service-prod", props);
 new WebsocScraperV2Stack(app, "peterportal-api-next-websoc-scraper-v2-prod", props);
---
+++ tools/cdk/websocProxyServiceStack.ts
@@ -0,0 +1,25 @@
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+export class WebsocProxyServiceStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
+    if (process.env.NODE_ENV !== "production")
+      throw new Error("Cannot deploy this stack outside of production. Stop.");
+    super(scope, id, props);
+    const functionName = "peterportal-api-next-prod-websoc-proxy-service";
+    new Function(this, functionName, {
+      code: Code.fromAsset(
+        join(dirname(fileURLToPath(import.meta.url)), "../websoc-proxy-service/dist")
+      ),
+      functionName,
+      handler: "index.lambdaHandler",
+      timeout: Duration.seconds(15),
+      runtime: Runtime.NODEJS_16_X,
+      memorySize: 512,
+      ...props,
+    });
+  }
+}
