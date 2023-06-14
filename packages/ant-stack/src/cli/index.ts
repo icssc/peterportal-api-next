6f1adcbc3180362b075f037268cc8ae7d3ec4e0d
Aponia <brianmv@uci.edu>
2023-06-14 16:09:07 -0700

    feat: PeterPortal API SST (#47)
    Co-authored-by: Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>

---
+++ packages/ant-stack/src/cli/index.ts
@@ -0,0 +1,74 @@
+import chalk from "chalk";
+import { cli, command } from "cleye";
+import { consola } from "consola";
+import { defu } from "defu";
+
+import { buildInternalHandler } from "./commands/build.js";
+import { automaticCreate, CreateOptions, interactiveCreate } from "./commands/create.js";
+import { startDevServer } from "./commands/dev.js";
+
+async function start() {
+  consola.log(chalk.green.bgRedBright(`ant - AntStack's CLI tool`));
+
+  const argv = cli({
+    name: "ant",
+
+    version: "0.1.0",
+
+    commands: [
+      command({
+        name: "create",
+        parameters: ["[endpoint]"],
+        flags: {
+          interactive: {
+            type: Boolean,
+            alias: "i",
+            description: "Interactively create a new endpoint",
+          },
+          methods: {
+            type: [String],
+          },
+        },
+      }),
+
+      command({
+        name: "dev",
+      }),
+
+      command({
+        name: "build",
+      }),
+    ],
+  });
+
+  switch (argv.command) {
+    case "create": {
+      if (argv.flags.interactive) {
+        return await interactiveCreate();
+      }
+
+      const createOptions = CreateOptions(defu(argv.flags, argv._));
+
+      if (!createOptions.data) {
+        consola.error(chalk.redBright(createOptions.problems));
+        return;
+      }
+
+      return await automaticCreate(createOptions.data);
+    }
+
+    case "build": {
+      return await buildInternalHandler();
+    }
+
+    case "dev": {
+      return await startDevServer();
+    }
+  }
+}
+
+start();
+
+export * from "./commands/build.js";
+export * from "./commands/create.js";
+export * from "./commands/dev.js";
---
+++ packages/ant-stack/tsup.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "tsup";
+
+/**
+ * @see https://github.com/evanw/esbuild/issues/1921#issuecomment-1491470829
+ */
+const js = `\
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire as topLevelCreateRequire } from 'module';
+const require = topLevelCreateRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+`;
+
+export default defineConfig({
+  entry: {
+    cli: "src/cli/index.ts",
+    config: "src/config.ts",
+    "lambda-core": "src/lambda-core/index.ts",
+  },
+  bundle: true,
+  format: "esm",
+  sourcemap: true,
+  dts: true,
+  splitting: false,
+  banner: { js },
+  clean: true,
+
+  /**
+   * Bundle **all** dependencies into the output files to prepare for Lambda deployment.
+   */
+  noExternal: [/^((?!esbuild).)*$/],
+});
