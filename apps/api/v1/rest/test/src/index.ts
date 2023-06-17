483b875505ef5caa8f551782424334941f714d5a
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-06-17 12:43:12 -0700

    feat: create test route

---
+++ apps/api/v1/rest/test/src/index.ts
@@ -0,0 +1,14 @@
+import { createErrorResult, createOKResult, type InternalHandler, zeroUUID } from "ant-stack";
+
+export const GET: InternalHandler = async (event) => {
+  return createOKResult({}, zeroUUID);
+};
+export const POST: InternalHandler = async (event) => {
+  return createOKResult({}, zeroUUID);
+};
+export const PUT: InternalHandler = async (event) => {
+  return createOKResult({}, zeroUUID);
+};
+export const DELETE: InternalHandler = async (event) => {
+  return createOKResult({}, zeroUUID);
+};
