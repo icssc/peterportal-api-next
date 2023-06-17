eef3a305328537236886455fa045dae6ef4cb733
senti <brianmv@uci.edu>
2023-06-17 16:41:05 -0700

    fix: dedupe the project paths found

--- packages/ant-stack/src/utils/searchProjects.ts
+++ packages/ant-stack/src/utils/searchProjects.ts
@@ -4,7 +4,16 @@ import { join } from "node:path";
 /**
  * Recursively find all paths to projects starting from a given root directory.
  */
-export function findAllProjects(root = ".", directory = "", paths: string[] = []): string[] {
+export function findAllProjects(root = "."): string[] {
+  const allProjects = findSubProjects(root);
+  const dedupedProjects = [...new Set(allProjects)];
+  return dedupedProjects;
+}
+
+/**
+ * Recursively find all paths to projects starting from a given root directory.
+ */
+export function findSubProjects(root = ".", directory = "", paths: string[] = []): string[] {
   if (existsSync(`${root}/${directory}/package.json`)) {
     paths.push(join(root, directory));
     return paths;
@@ -14,5 +23,5 @@ export function findAllProjects(root = ".", directory = "", paths: string[] = []
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
 
-  return subRoutes.flatMap((subRoute) => findAllProjects(root, `${directory}/${subRoute}`, paths));
+  return subRoutes.flatMap((subRoute) => findSubProjects(root, `${directory}/${subRoute}`, paths));
 }
