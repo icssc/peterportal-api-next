3e5ac2a6d8d0d183e0ab8311cdb04b76a2ef969b
senti <brianmv@uci.edu>
2023-07-06 14:43:48 -0700

    feat: mostly done with entry file

--- apps/api/v1/graphql/src/index.ts
+++ apps/api/v1/graphql/src/index.ts
@@ -9,6 +9,7 @@ import {
 import { handlers, startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs } from "@graphql-tools/merge";
+import { createOKResult, type InternalHandler, type InternalRequest } from "ant-stack";
 import { getClosestProjectDirectory } from "ant-stack/utils";
 
 const projectDirectory = getClosestProjectDirectory();
@@ -24,7 +25,11 @@ const graphqlServer = new ApolloServer({
   typeDefs: mergeTypeDefs(loadFilesSync(path.join(projectDirectory, "src", "graphql/*.graphql"))),
 });
 
-export function createGraphqlExpressMiddleware() {
+/**
+ * "The inferred type of ... cannot be named without a reference ..."
+ * @see https://github.com/microsoft/TypeScript/issues/42873
+ */
+export function createGraphqlExpressMiddleware(): ReturnType<typeof expressMiddleware> {
   graphqlServer.start().catch(() => []);
   return expressMiddleware(graphqlServer);
 }
@@ -33,21 +38,32 @@ export function createGraphqlExpressMiddleware() {
  * "The inferred type of ... cannot be named without a reference ..."
  * @see https://github.com/microsoft/TypeScript/issues/42873
  */
-export const lambdaHandler = startServerAndCreateLambdaHandler(
-  graphqlServer,
-  handlers.createAPIGatewayProxyEventRequestHandler(),
-  {
-    middleware: [
-      async () => {
-        return async (res) => {
-          res.headers = {
-            ...res.headers,
-            "Access-Control-Allow-Headers": "Apollo-Require-Preflight, Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+export const lambdaHandler: ReturnType<typeof startServerAndCreateLambdaHandler> =
+  startServerAndCreateLambdaHandler(
+    graphqlServer,
+    handlers.createAPIGatewayProxyEventRequestHandler(),
+    {
+      middleware: [
+        async () => {
+          return async (res) => {
+            res.headers = {
+              ...res.headers,
+              "Access-Control-Allow-Headers": "Apollo-Require-Preflight, Content-Type",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            };
           };
-        };
-      },
-    ],
-  }
-);
+        },
+      ],
+    }
+  );
+
+/**
+ * {@link InternalHandler} only receives an {@link InternalRequest}, not context or callback from AWS Lambda.
+ */
+const placeholder: any = {};
+
+export const ALL: InternalHandler = async (request) => {
+  const lambdaHandlerResponse = await lambdaHandler(request, placeholder, placeholder);
+  return createOKResult(lambdaHandlerResponse, request.requestId);
+};
