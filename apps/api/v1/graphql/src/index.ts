ae7e83ade3a734cdd276f3f2ed073d43666111d9
senti <brianmv@uci.edu>
2023-07-06 14:33:47 -0700

    feat: started

--- apps/api/v1/graphql/src/index.ts
+++ apps/api/v1/graphql/src/index.ts
@@ -1,5 +1,4 @@
 import path from "node:path";
-import url from "node:url";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
@@ -10,3 +9,45 @@ import {
 import { handlers, startServerAndCreateLambdaHandler } from "@as-integrations/aws-lambda";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs } from "@graphql-tools/merge";
+import { getClosestProjectDirectory } from "ant-stack/utils";
+
+const projectDirectory = getClosestProjectDirectory();
+
+const graphqlServer = new ApolloServer({
+  introspection: true,
+  plugins: [
+    process.env.NODE_ENV === "development"
+      ? ApolloServerPluginLandingPageLocalDefault()
+      : ApolloServerPluginLandingPageProductionDefault({ footer: false }),
+  ],
+  // resolvers,
+  typeDefs: mergeTypeDefs(loadFilesSync(path.join(projectDirectory, "src", "graphql/*.graphql"))),
+});
+
+export function createGraphqlExpressMiddleware() {
+  graphqlServer.start().catch(() => []);
+  return expressMiddleware(graphqlServer);
+}
+
+/**
+ * "The inferred type of ... cannot be named without a reference ..."
+ * @see https://github.com/microsoft/TypeScript/issues/42873
+ */
+export const lambdaHandler = startServerAndCreateLambdaHandler(
+  graphqlServer,
+  handlers.createAPIGatewayProxyEventRequestHandler(),
+  {
+    middleware: [
+      async () => {
+        return async (res) => {
+          res.headers = {
+            ...res.headers,
+            "Access-Control-Allow-Headers": "Apollo-Require-Preflight, Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          };
+        };
+      },
+    ],
+  }
+);
