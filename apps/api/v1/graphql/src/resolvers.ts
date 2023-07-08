93939cf62c64ca0beb828035215982d4c1c450bb
senti <brianmv@uci.edu>
2023-07-08 15:56:30 -0700

    fix: env.stage is lower case?

--- apps/api/v1/graphql/src/resolvers.ts
+++ apps/api/v1/graphql/src/resolvers.ts
@@ -3,10 +3,14 @@ import type { IFieldResolver } from "@graphql-tools/utils";
 import { GraphQLError } from "graphql/error";
 import { isErrorResponse, type RawResponse } from "peterportal-api-next-types";
 
+/**
+ * Convention: either the route provided to {@link proxyRestApi} should start with a slash,
+ * or the fetch request should add a slash. For now, the latter will be used
+ */
 export const resolvers: ApolloServerOptions<BaseContext>["resolvers"] = {
   Query: {
-    calendar: proxyRestApi("/v1/rest/calendar"),
-    websoc: proxyRestApi("/v1/rest/websoc"),
+    calendar: proxyRestApi("v1/rest/calendar"),
+    websoc: proxyRestApi("v1/rest/websoc"),
   },
 };
 
@@ -22,7 +26,7 @@ export function proxyRestApi(route: string): IFieldResolver<never, BaseContext> 
       .then((res) => res.json())
       .catch((err) => {
         return {
-          error: "INTERNAL_SERVER_ERROR",
+          error: `INTERNAL_SERVER_ERROR: ${err.message}`,
           message: err.message,
         };
       });
@@ -50,6 +54,6 @@ function getBaseUrl() {
 
     case "staging":
     default:
-      return `https://${process.env.STAGE}.api-next.peterportal.org`;
+      return `https://${process.env.STAGE ?? process.env.stage}.api-next.peterportal.org`;
   }
 }
