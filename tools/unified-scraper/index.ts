0252864176c5732eaa3eb6161036586c10b7869c
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-07-16 22:04:38 -0700

    chore: remove prisma debug

--- tools/unified-scraper/index.ts
+++ tools/unified-scraper/index.ts
@@ -12,32 +12,7 @@ import {
   upsertCourses,
 } from "./lib";
 
-const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-    {
-      emit: "stdout",
-      level: "error",
-    },
-    {
-      emit: "stdout",
-      level: "info",
-    },
-    {
-      emit: "stdout",
-      level: "warn",
-    },
-  ],
-});
-
-prisma.$on("query", (e) => {
-  console.log("Query: " + e.query);
-  console.log("Params: " + e.params);
-  console.log("Duration: " + e.duration + "ms");
-});
+const prisma = new PrismaClient();
 
 async function main() {
   const courseInfo = JSON.parse(
