9e07ac5b63150731188d23b634a99381d7f0bf4f
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-07-23 14:33:49 -0700

    chore: update template

--- packages/ant-stack/src/templates/index.ts
+++ packages/ant-stack/src/templates/index.ts
@@ -1,21 +1,21 @@
 /**
- * This file is not actually used. But is an example of an entry file.
+ * This file is not actually used, but rather an example of a route's entry point.
  */
 
 import { createOKResult, type InternalHandler, zeroUUID } from "ant-stack";
 
-export const GET: InternalHandler = async (event) => {
-  return createOKResult(event.body, zeroUUID);
+export const GET: InternalHandler = async (request) => {
+  return createOKResult(request.body, request.headers, zeroUUID);
 };
 
-export const POST: InternalHandler = async (event) => {
-  return createOKResult(event.body, zeroUUID);
+export const POST: InternalHandler = async (request) => {
+  return createOKResult(request.body, request.headers, zeroUUID);
 };
 
-export const PUT: InternalHandler = async (event) => {
-  return createOKResult(event.body, zeroUUID);
+export const PUT: InternalHandler = async (request) => {
+  return createOKResult(request.body, request.headers, zeroUUID);
 };
 
-export const DELETE: InternalHandler = async (event) => {
-  return createOKResult(event.body, zeroUUID);
+export const DELETE: InternalHandler = async (request) => {
+  return createOKResult(request.body, request.headers, zeroUUID);
 };
