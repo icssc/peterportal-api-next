ce1e5dc6a073d3264c32a0d19a9ca876e1a4c681
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-08-04 18:12:13 -0700

    feat: implement arbitrary filtering for instructors

--- apps/api/v1/rest/instructors/src/index.ts
+++ apps/api/v1/rest/instructors/src/index.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@libs/db";
 import { createErrorResult, createOKResult } from "ant-stack";
 import type { InternalHandler } from "ant-stack";
+import { ZodError } from "zod";
+
+import { constructPrismaQuery } from "./lib";
+import { QuerySchema } from "./schema";
 
 let prisma: PrismaClient;
 
 export const GET: InternalHandler = async (request) => {
-  const { headers, params, requestId } = request;
+  const { headers, params, query, requestId } = request;
 
   prisma ??= new PrismaClient();
 
@@ -35,7 +39,34 @@ export const GET: InternalHandler = async (request) => {
       return createErrorResult(404, `Instructor ${params.id} not found`, requestId);
     }
   } else {
-    // TODO implement arbitrary filtering
-    return createErrorResult(400, "Instructor UCInetID not provided", requestId);
+    try {
+      const parsedQuery = QuerySchema.parse(query);
+      // The query object being empty shouldn't return all courses, since there's /courses/all for that.
+      if (!Object.keys(parsedQuery).length)
+        return createErrorResult(400, "Instructor UCInetID not provided", requestId);
+      const instructors = await prisma.instructor.findMany({
+        where: constructPrismaQuery(parsedQuery),
+      });
+      if (parsedQuery.taughtInTerms) {
+        const terms = new Set(parsedQuery.taughtInTerms);
+        return createOKResult(
+          instructors.filter(
+            (instructor) =>
+              [...new Set(Object.values(instructor.courseHistory as Record<string, string[]>))]
+                .flat()
+                .filter((x) => terms.has(x)).length,
+          ),
+          headers,
+          requestId,
+        );
+      }
+      return createOKResult(instructors, headers, requestId);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const messages = error.issues.map((issue) => issue.message);
+        return createErrorResult(400, messages.join("; "), requestId);
+      }
+      return createErrorResult(400, error, requestId);
+    }
   }
 };
