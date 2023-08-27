de61485eca1cd6868c65b2d5680eb51fac0c3948
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-08-27 14:03:59 -0700

    feat: :sparkles: add GraphQL support

--- apps/api/v1/graphql/src/resolvers.ts
+++ apps/api/v1/graphql/src/resolvers.ts
@@ -15,6 +15,7 @@ export const resolvers: ApolloServerOptions<BaseContext>["resolvers"] = {
     rawGrades: proxyRestApi("v1/rest/grades/raw"),
     aggregateGrades: proxyRestApi("v1/rest/grades/aggregate"),
     gradesOptions: proxyRestApi("v1/rest/grades/options"),
+    aggregateGroupedGrades: proxyRestApi("v1/rest/grades/aggregateGrouped"),
     instructor: proxyRestApi("v1/rest/instructors", { pathArg: "courseId" }),
     instructors: proxyRestApi("v1/rest/instructors"),
     allInstructors: proxyRestApi("v1/rest/instructors/all"),
