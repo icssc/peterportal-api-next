156548266485ede5284fe2c2e065d561ade202af
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-08-30 18:46:59 -0700

    feat: ✨ typescriptify websoc-fuzzy-search [skip ci]

--- packages/websoc-fuzzy-search/public/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { SearchParams, SearchResult } from "./types";
-
-export default function search(params?: SearchParams): Record<string, SearchResult>;
-export * from "./types";
--- packages/websoc-fuzzy-search/src/helpers.ts
+++ packages/websoc-fuzzy-search/src/helpers.ts
@@ -1,13 +1,20 @@
-import index from "../output/index.js";
+import { index } from "../output";
 
-import { types, fieldNames, courseFieldNames, instructorFieldNames } from "./constants.js";
-import { matchCourseNum, tokenizeCourseNum } from "./regex.js";
+import { types, fieldNames, courseFieldNames, instructorFieldNames } from "./constants";
+import { matchCourseNum, tokenizeCourseNum } from "./regex";
+import {
+  CourseMetadata,
+  FilterOptions,
+  InstructorMetadata,
+  ResultType,
+  SearchResult,
+} from "./types";
 
-// comparation function for sorting responses
-function compare(a, b) {
+// comparison function for sorting responses
+function compare(a: string, b: string) {
   // compare object types in the order GE->department->course->instructor
-  let aType = index.objects[a][0];
-  let bType = index.objects[b][0];
+  const aType = index.objects[a][0];
+  const bType = index.objects[b][0];
   if (aType !== bType) return Math.sign(types[bType] - types[aType]);
   // special ordering for course numbers that checks in the order department->numeral->prefix->suffix
   if (aType === "COURSE") {
@@ -23,7 +30,7 @@ function compare(a, b) {
       if (aNum === bNum) {
         return aPre === bPre ? lexOrd(aSuf, bSuf) : lexOrd(aPre, bPre);
       }
-      return lexOrd(parseInt(aNum), parseInt(bNum));
+      return lexOrd(parseInt(aNum as string), parseInt(bNum as string));
     }
     return lexOrd(aDept, bDept);
   }
@@ -32,50 +39,70 @@ function compare(a, b) {
 }
 
 // shorthand for the lexicographical ordering ternary
-function lexOrd(a, b) {
+function lexOrd<T>(a: T, b: T) {
   return a === b ? 0 : a < b ? -1 : 1;
 }
 
 // given an array of keys, return a mapping of those keys to their results in index.objects
-export function expandResponse(response, numResults, resultTypes, filterOptions) {
+export function expandResponse(
+  response?: string[],
+  numResults?: number,
+  resultTypes?: ResultType,
+  filterOptions?: FilterOptions,
+) {
   response = resultTypes
-    ? response.filter((x) => resultTypes.includes(index.objects[x][0]))
+    ? response?.filter((x) => resultTypes.includes(index.objects[x][0]))
     : response;
   if (filterOptions) {
     for (const [k, v] of Object.entries(filterOptions)) {
       if (!v.length) continue;
-      response = response.filter(
-        (x) => index.objects[x][2][k] && v.every((y) => index.objects[x][2][k].includes(y)),
+      response = response?.filter(
+        (x) => index.objects[x][2][k] && v.every((y: string) => index.objects[x][2][k].includes(y)),
       );
     }
   }
   return response
-    .sort(compare)
+    ?.sort(compare)
     .slice(0, numResults)
-    .reduce((obj, key) => {
-      obj[key] = index.objects[key].reduce((prev, curr, index) => {
-        prev[fieldNames[index]] = curr;
-        return prev;
-      }, {});
-      if (obj[key].type === "COURSE" || obj[key].type === "INSTRUCTOR") {
-        obj[key].metadata = obj[key].metadata.reduce((prev, curr, index) => {
-          prev[(obj[key].type === "COURSE" ? courseFieldNames : instructorFieldNames)[index]] =
-            curr;
-          return prev;
-        }, {});
-      }
-      return obj;
-    }, {});
+    .reduce(
+      (obj, key) => {
+        obj[key] = index.objects[key].reduce(
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          (prev, curr, index) => {
+            prev[fieldNames[index]] = curr;
+            return prev;
+          },
+          {} as SearchResult,
+        );
+        if (obj[key].type === "COURSE" || obj[key].type === "INSTRUCTOR") {
+          obj[key].metadata = (
+            obj[key].metadata as unknown as Array<CourseMetadata | InstructorMetadata>
+          ).reduce(
+            (prev, curr, index) => {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              prev[(obj[key].type === "COURSE" ? courseFieldNames : instructorFieldNames)[index]] =
+                curr;
+              return prev;
+            },
+            {} as CourseMetadata | InstructorMetadata,
+          );
+        }
+        return obj;
+      },
+      {} as Record<string, SearchResult>,
+    );
 }
 
 // search on a single course number, with or without department
-export function searchCourseNumber(courseNum) {
-  let response = [];
-  const matchGroups = courseNum.match(matchCourseNum).groups;
+export function searchCourseNumber(courseNum: string) {
+  const response = [];
+  const matchGroups = courseNum.match(matchCourseNum)?.groups;
   // next check if a department was matched
-  if (matchGroups.department) {
+  if (matchGroups?.department) {
     for (const [alias, department] of Object.entries(index.aliases)) {
-      for (const dept of department) {
+      for (const dept of department as string[]) {
         courseNum = courseNum.replace(
           new RegExp(`^${alias}(?=[bcdehmnps]?\\d{1,3}[a-z]{0,4})`),
           dept.toString(),
@@ -93,7 +120,7 @@ export function searchCourseNumber(courseNum) {
       ...Object.keys(index.objects).filter(
         (x) =>
           index.objects[x][0] === "COURSE" &&
-          index.objects[x][2][1].includes(matchGroups.number.toUpperCase()),
+          index.objects[x][2][1].includes(matchGroups?.number.toUpperCase()),
       ),
     );
   }
@@ -101,7 +128,7 @@ export function searchCourseNumber(courseNum) {
 }
 
 // search on a single GE category
-export function searchGECategory(geCategory) {
+export function searchGECategory(geCategory: string) {
   return [
     geCategory,
     ...Object.keys(index.objects).filter(
@@ -114,17 +141,22 @@ export function searchGECategory(geCategory) {
 }
 
 // search on a single keyword
-export function searchKeyword(keyword, numResults) {
+export function searchKeyword(keyword: string, numResults?: number) {
   keyword = keyword.toLowerCase();
-  let response = [];
+  const response = [];
   // match all keywords
   const keyArrMap = Object.keys(index.keywords)
     .filter((x) => x.includes(keyword))
-    .sort((a, b) => (a.length === b.length ? lexOrd(a, b) : lexOrd(a.length, b.length)))
-    .reduce((obj, val) => {
-      obj[val] = index.keywords[val];
-      return obj;
-    }, {});
+    .sort((a, b) =>
+      a.length === b.length ? lexOrd(a, b) : lexOrd(a.length.toString(), b.length.toString()),
+    )
+    .reduce(
+      (obj, val) => {
+        obj[val] = index.keywords[val];
+        return obj;
+      },
+      {} as Record<string, string[]>,
+    );
   // prioritize exact keyword matches
   let exactDeptMatch = false;
   for (const kw of Object.keys(keyArrMap)) {
@@ -153,7 +185,10 @@ export function searchKeyword(keyword, numResults) {
   if (!exactDeptMatch) response.push(...Object.values(keyArrMap).flat());
   // if there are bare departments and not enough responses, add the courses from that department
   for (const key of response) {
-    if (index.objects[key][0] === "DEPARTMENT" && response.length <= numResults) {
+    if (
+      index.objects[key][0] === "DEPARTMENT" &&
+      response.length <= (numResults ?? Number.MAX_SAFE_INTEGER)
+    ) {
       response.push(
         ...Object.keys(index.objects).filter(
           (x) => index.objects[x][2] && index.objects[x][2][0] === key,
