5cbbb623f8c715241d3632c25c8d6a62de117618
Aponia <brianmv@uci.edu>
2023-09-06 17:25:30 -0700

    feat: ✨ api-fourmilier and ant-stack lambda shims

---
+++ apps/api-fourmilier/bronya.config.ts
@@ -0,0 +1,124 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+import path from "node:path";
+import fs from "node:fs";
+import { App, Stack } from "aws-cdk-lib/core";
+import { isCdk } from "@bronya.js/core";
+import { Api } from "@bronya.js/api-construct";
+import { createApiCliPlugins } from "@bronya.js/api-construct/plugins/cli";
+
+/**
+ * @see https://github.com/evanw/esbuild/issues/1921#issuecomment-1491470829
+ */
+const js = `\
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire as topLevelCreateRequire } from 'module';
+const require = topLevelCreateRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+`;
+
+const projectRoot = process.cwd();
+
+/**
+ * Where @libs/db is located.
+ */
+const libsDbDirectory = path.resolve(projectRoot, "..", "..", "libs", "db");
+
+const prismaClientDirectory = path.resolve(libsDbDirectory, "node_modules", "prisma");
+
+const prismaSchema = path.resolve(libsDbDirectory, "prisma", "schema.prisma");
+
+class MyStack extends Stack {
+  public api: Api;
+  constructor(scope: App, id: string) {
+    super(scope, id);
+
+    this.api = new Api(this, `${id}-api`, {
+      plugins: createApiCliPlugins(),
+      exitPoint: "handler.mjs",
+      constructs: {
+        lambdaUpload(directory) {
+          const queryEngines = fs.readdirSync(directory).filter((x) => x.endsWith(".so.node"));
+
+          if (queryEngines.length === 1) {
+            return;
+          }
+
+          queryEngines
+            .filter((x) => x !== "libquery_engine-linux-arm64-openssl-1.0.x.so.node")
+            .forEach((queryEngineFile) => {
+              fs.rmSync(path.join(directory, queryEngineFile));
+            });
+        },
+      },
+      environment: {
+        DATABASE_URL: process.env["DATABASE_URL"] ?? "",
+        SHADOW_DATABASE_URL: process.env["SHADOW_DATABASE_URL"] ?? "",
+      },
+      esbuild: {
+        format: "esm",
+        platform: "node",
+        bundle: true,
+        banner: { js },
+        outExtension: { ".js": ".mjs" },
+        plugins: [
+          {
+            name: "copy",
+            setup(build) {
+              build.onStart(async () => {
+                const outDirectory = build.initialOptions.outdir ?? projectRoot;
+
+                fs.mkdirSync(outDirectory, { recursive: true });
+
+                const queryEngines = fs
+                  .readdirSync(prismaClientDirectory)
+                  .filter((file) => file.endsWith(".so.node"));
+
+                queryEngines.forEach((queryEngineFile) =>
+                  fs.copyFileSync(
+                    path.join(prismaClientDirectory, queryEngineFile),
+                    path.join(outDirectory, queryEngineFile),
+                  ),
+                );
+
+                queryEngines.forEach((queryEngineFile) =>
+                  fs.chmodSync(path.join(outDirectory, queryEngineFile), 0o755),
+                );
+
+                fs.copyFileSync(prismaSchema, path.join(outDirectory, "schema.prisma"));
+              });
+            },
+          },
+        ],
+      },
+    });
+  }
+}
+
+export async function main() {
+  const app = new App();
+
+  const stack = new MyStack(app, "peterportal-api-canary");
+
+  const api = stack.api;
+
+  await api.init();
+
+  if (isCdk()) {
+    const result = await api.synth();
+
+    result.api.addGatewayResponse;
+
+    result.functions;
+  }
+
+  return app;
+}
+
+if (isCdk()) {
+  main();
+}
---
+++ libs/lambda/src/response.ts
@@ -0,0 +1,96 @@
+import type { ErrorResponse, Response } from "@peterportal-api/types";
+import type { APIGatewayProxyResult } from "aws-lambda";
+
+import { compress } from "./compress";
+import { httpErrorCodes, months } from "./constants";
+import { logger } from "./logger";
+
+/**
+ * Common response headers.
+ */
+const responseHeaders: Record<string, string> = {
+  "Access-Control-Allow-Headers": "Apollo-Require-Preflight, Content-Type",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+};
+
+/**
+ * Helper function for converting a ``Date`` object to a CLF date string.
+ * @example `11/Dec/2022:03:48:12 +0000`
+ *
+ * This only exists because API Gateway's gateway responses can only return the
+ * timestamp in this format, and not ISO 8601.
+ *
+ * @param date The ``Date`` object to convert.
+ */
+export function createTimestamp(date: Date = new Date()): string {
+  const day = date.getUTCDate().toString().padStart(2, "0");
+  const month = months[date.getUTCMonth()];
+  const year = date.getUTCFullYear();
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = date.getUTCSeconds().toString().padStart(2, "0");
+
+  return `${day}/${month}/${year}:${hours}:${minutes}:${seconds} +0000`;
+}
+
+/**
+ * Log and create a "200 OK" response.
+ * @param payload The payload to send in the response.
+ * @param requestHeaders
+ * @param requestId The request ID associated with the request.
+ */
+export function createOKResult<T>(
+  payload: T,
+  requestHeaders: Record<string, string>,
+  requestId: string,
+): APIGatewayProxyResult {
+  const statusCode = 200;
+  const timestamp = createTimestamp();
+  const response: Response<T> = { statusCode, timestamp, requestId, payload };
+  const headers = { ...responseHeaders };
+  try {
+    const { body, method } = compress(JSON.stringify(response), requestHeaders["accept-encoding"]);
+    if (method) headers["Content-Encoding"] = method;
+    logger.info("200 OK");
+    return {
+      statusCode,
+      isBase64Encoded: !!headers["Content-Encoding"],
+      body,
+      headers,
+    };
+  } catch (e) {
+    return createErrorResult(500, e, requestId);
+  }
+}
+
+/**
+ * Log and create an error response.
+ *
+ * @param statusCode The status code to send in the response.
+ * @param e The error to send in the response.
+ *
+ * @param requestId The request ID associated with the request.
+ */
+export function createErrorResult(
+  statusCode: number,
+  e: unknown,
+  requestId: string,
+): APIGatewayProxyResult {
+  const timestamp = createTimestamp();
+
+  const message =
+    e instanceof Error
+      ? `${e.name}: ${e.message}`
+      : typeof e === "string"
+      ? e
+      : "An unknown error has occurred. Please try again.";
+
+  const error = httpErrorCodes[statusCode as keyof typeof httpErrorCodes];
+
+  const body: ErrorResponse = { timestamp, requestId, statusCode, error, message };
+
+  logger.error(`${body.statusCode} ${body.error}: ${body.message}`);
+
+  return { statusCode, body: JSON.stringify(body), headers: responseHeaders };
+}
