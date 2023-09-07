d9c893829041093e61df2ab628b6f8e97c02692b
Aponia <brianmv@uci.edu>
2023-09-06 21:30:40 -0700

    feat: ✨ courses endpoint

---
+++ apps/api-fourmilier/src/v1/rest/courses/schema.ts
@@ -0,0 +1,24 @@
+import { flattenStringsAndSplit } from "@libs/utils";
+import { anyArray, divisionCodes, geCodes } from "@peterportal-api/types";
+import { z } from "zod";
+
+export const QuerySchema = z.object({
+  department: z.string().optional(),
+  courseNumber: z.string().optional(),
+  courseNumeric: z.coerce.number().int().optional(),
+  titleContains: z.string().optional(),
+  courseLevel: z.enum(anyArray).or(z.enum(divisionCodes)).optional(),
+  minUnits: z.coerce.number().optional(),
+  maxUnits: z.coerce.number().optional(),
+  descriptionContains: z.string().optional(),
+  taughtByInstructors: z
+    .string()
+    .array()
+    .or(z.string())
+    .optional()
+    .transform(flattenStringsAndSplit),
+  geCategory: z.enum(anyArray).or(z.enum(geCodes)).optional(),
+  taughtInTerms: z.string().array().or(z.string()).optional().transform(flattenStringsAndSplit),
+});
+
+export type Query = z.infer<typeof QuerySchema>;
