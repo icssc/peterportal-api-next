ca5ce30980e5bd7572ed2890918896686d610d47
Aponia <brianmv@uci.edu>
2023-09-06 21:34:55 -0700

    feat: ✨ grades endpoint

---
+++ apps/api-fourmilier/src/v1/rest/grades/{id}/schema.ts
@@ -0,0 +1,24 @@
+import { anyArray, divisionCodes, quarters } from "@peterportal-api/types";
+import { z } from "zod";
+
+export const QuerySchema = z.object({
+  year: z
+    .string()
+    .regex(/^\d{4}$/, { message: "Invalid year provided" })
+    .optional(),
+  quarter: z.enum(quarters, { invalid_type_error: "Invalid quarter provided" }).optional(),
+  instructor: z.string().optional(),
+  department: z.string().optional(),
+  courseNumber: z.string().optional(),
+  sectionCode: z
+    .string()
+    .regex(/^\d{5}$/, { message: "Invalid sectionCode provided" })
+    .optional(),
+  division: z.enum(anyArray).or(z.enum(divisionCodes)).optional(),
+  excludePNP: z
+    .string()
+    .optional()
+    .transform((x) => x === "true"),
+});
+
+export type Query = z.infer<typeof QuerySchema>;
