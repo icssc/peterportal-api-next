03169b87d67e76f4ef56974b1d8e5bd5efacfdb5
Aponia <brianmv@uci.edu>
2023-10-15 06:20:25 -0700

    style: 🎨 add more documentation

--- apps/api/bronya.config.ts
+++ apps/api/bronya.config.ts
@@ -16,6 +16,24 @@ import { App, Stack, Duration } from "aws-cdk-lib/core";
 import { config } from "dotenv";
 import type { BuildOptions } from "esbuild";
 
+/**
+ * Whether or not we're executing in CDK.
+ *
+ * During development (not in CDK) ...
+ * - Bronya is responsible for handling the development server.
+ * - The exported `main` function is imported and then called to get the CDK app.
+ * - No AWS constructs are actually allocated, since this requires certain files to be built that
+ *   may not exist during development.
+ *
+ * During deployment (in CDK) ...
+ * - The `cdk` CLI tool is responsible for deploying the CloudFormation stack.
+ * - The CLI expects the app to be created at the top level,
+ *   so the main function is also called immediately after its declaration.
+ * - All the AWS constructs are allocated, and relevant transformations are applied before
+ *   uploading the code via AWS CloudFormation.
+ */
+const executingInCdk = isCdk();
+
 config({
   path: "../../.env",
 });
@@ -135,16 +153,20 @@ function getStage() {
   if (!process.env.NODE_ENV) {
     throw new Error("NODE_ENV not set.");
   }
+
   switch (process.env.NODE_ENV) {
     case "production":
       return "prod";
+
     case "staging":
       if (!process.env.PR_NUM) {
         throw new Error("NODE_ENV was set to staging, but a PR number was not provided.");
       }
       return `staging-${process.env.PR_NUM}`;
+
     case "development":
       return "dev";
+
     default:
       throw new Error(
         "Invalid NODE_ENV specified. Valid values are 'production', 'staging', and 'development'.",
@@ -152,7 +174,11 @@ function getStage() {
   }
 }
 
-export async function main() {
+/**
+ * Bronya requires a default export or exported named `main` function that returns an {@link App}
+ * in order to support development functionality.
+ */
+export async function main(): Promise<App> {
   const id = "peterportal-api-next";
 
   const zoneName = "peterportal.org";
@@ -163,94 +189,100 @@ export async function main() {
 
   const stack = new ApiStack(app, `${id}-${stage}`, stage);
 
-  const api = stack.api;
+  await stack.api.init();
 
-  await api.init();
-
-  if (isCdk()) {
-    if (stage === "dev") {
-      throw new Error("Cannot deploy this app in the development environment.");
-    }
-
-    const result = await api.synth();
-
-    /**
-     * Add gateway responses for 5xx and 404 errors, so that they remain compliant
-     * with the {@link `ErrorResponse`} type.
-     */
-    result.api.addGatewayResponse(`${id}-${stage}-5xx`, {
-      type: ResponseType.DEFAULT_5XX,
-      statusCode: "500",
-      templates: {
-        "application/json": JSON.stringify({
-          timestamp: "$context.requestTime",
-          requestId: "$context.requestId",
-          statusCode: 500,
-          error: "Internal Server Error",
-          message: "An unknown error has occurred. Please try again.",
-        }),
-      },
-    });
-    result.api.addGatewayResponse(`${id}-${stage}-404`, {
-      type: ResponseType.MISSING_AUTHENTICATION_TOKEN,
-      statusCode: "404",
-      templates: {
-        "application/json": JSON.stringify({
-          timestamp: "$context.requestTime",
-          requestId: "$context.requestId",
-          statusCode: 404,
-          error: "Not Found",
-          message: "The requested resource could not be found.",
-        }),
-      },
-    });
-
-    /**
-     * Define the CORS response headers integration and add it to all endpoints.
-     * This is necessary since we hacked API Gateway to be able to serve binary data.
-     */
-    const corsIntegration = new LambdaIntegration(
-      new AwsLambdaFunction(result.api, `${id}-${stage}-options-handler`, {
-        code: Code.fromInline(
-          // language=JavaScript
-          'exports.h=async _=>({headers:{"Access-Control-Allow-Origin": "*","Access-Control-Allow-Headers": "Apollo-Require-Preflight,Content-Type","Access-Control-Allow-Methods": "GET,POST,OPTIONS"},statusCode:204})',
-        ),
-        handler: "index.h",
-        runtime: Runtime.NODEJS_18_X,
-        architecture: Architecture.ARM_64,
-      }),
-    );
-    result.api.methods.forEach((apiMethod) => {
-      try {
-        apiMethod.resource.addMethod("OPTIONS", corsIntegration);
-      } catch {
-        // no-op
-      }
-    });
-
-    // Set up the custom domain name and A record for the API.
-    result.api.addDomainName(`${id}-${stage}-domain`, {
-      domainName: `${stage === "prod" ? "" : `${stage}.`}api-next.peterportal.org`,
-      certificate: Certificate.fromCertificateArn(
-        result.api,
-        "peterportal-cert",
-        process.env.CERTIFICATE_ARN ?? "",
-      ),
-    });
-    new ARecord(result.api, `${id}-${stage}-a-record`, {
-      zone: HostedZone.fromHostedZoneAttributes(result.api, "peterportal-hosted-zone", {
-        zoneName,
-        hostedZoneId: process.env.HOSTED_ZONE_ID ?? "",
-      }),
-      recordName: `${stage === "prod" ? "" : `${stage}.`}api-next`,
-      target: RecordTarget.fromAlias(new ApiGateway(result.api)),
-    });
+  // In development mode, return the app so that Bronya can handle the development server.
+  // The app is not synthesized, and no AWS resources are allocated.
+  if (!executingInCdk) {
+    return app;
   }
+
+  if (stage === "dev") {
+    throw new Error("Cannot deploy this app in the development environment.");
+  }
+
+  // In deployment mode, synthesize all the AWS resources.
+  const synthesized = await stack.api.synth();
+
+  /**
+   * Add gateway responses for 5xx and 404 errors, so that they remain compliant
+   * with the {@link `ErrorResponse`} type.
+   */
+  synthesized.api.addGatewayResponse(`${id}-${stage}-5xx`, {
+    type: ResponseType.DEFAULT_5XX,
+    statusCode: "500",
+    templates: {
+      "application/json": JSON.stringify({
+        timestamp: "$context.requestTime",
+        requestId: "$context.requestId",
+        statusCode: 500,
+        error: "Internal Server Error",
+        message: "An unknown error has occurred. Please try again.",
+      }),
+    },
+  });
+
+  synthesized.api.addGatewayResponse(`${id}-${stage}-404`, {
+    type: ResponseType.MISSING_AUTHENTICATION_TOKEN,
+    statusCode: "404",
+    templates: {
+      "application/json": JSON.stringify({
+        timestamp: "$context.requestTime",
+        requestId: "$context.requestId",
+        statusCode: 404,
+        error: "Not Found",
+        message: "The requested resource could not be found.",
+      }),
+    },
+  });
+
+  /**
+   * Define the CORS response headers integration and add it to all endpoints.
+   * This is necessary since we hacked API Gateway to be able to serve binary data.
+   */
+  const corsIntegration = new LambdaIntegration(
+    new AwsLambdaFunction(synthesized.api, `${id}-${stage}-options-handler`, {
+      code: Code.fromInline(
+        // language=JavaScript
+        'exports.h=async _=>({headers:{"Access-Control-Allow-Origin": "*","Access-Control-Allow-Headers": "Apollo-Require-Preflight,Content-Type","Access-Control-Allow-Methods": "GET,POST,OPTIONS"},statusCode:204})',
+      ),
+      handler: "index.h",
+      runtime: Runtime.NODEJS_18_X,
+      architecture: Architecture.ARM_64,
+    }),
+  );
+
+  synthesized.api.methods.forEach((apiMethod) => {
+    try {
+      apiMethod.resource.addMethod("OPTIONS", corsIntegration);
+    } catch {
+      // no-op
+    }
+  });
+
+  // Set up the custom domain name and A record for the API.
+  synthesized.api.addDomainName(`${id}-${stage}-domain`, {
+    domainName: `${stage === "prod" ? "" : `${stage}.`}api-next.peterportal.org`,
+    certificate: Certificate.fromCertificateArn(
+      synthesized.api,
+      "peterportal-cert",
+      process.env.CERTIFICATE_ARN ?? "",
+    ),
+  });
+
+  new ARecord(synthesized.api, `${id}-${stage}-a-record`, {
+    zone: HostedZone.fromHostedZoneAttributes(synthesized.api, "peterportal-hosted-zone", {
+      zoneName,
+      hostedZoneId: process.env.HOSTED_ZONE_ID ?? "",
+    }),
+    recordName: `${stage === "prod" ? "" : `${stage}.`}api-next`,
+    target: RecordTarget.fromAlias(new ApiGateway(synthesized.api)),
+  });
 
   return app;
 }
 
-if (isCdk()) {
+if (executingInCdk) {
   // Sike, looks like even though we have top-level await, the dev server won't start with it :(
   main().then();
 }
