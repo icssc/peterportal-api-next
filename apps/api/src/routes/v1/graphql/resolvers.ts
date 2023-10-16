62853c31460ae2d0681cc1aa04117a8bb80211e6
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-10-16 11:15:36 -0700

    fix(graphql): 🐛 add leading slash to proxy routes

--- apps/api/src/routes/v1/graphql/resolvers.ts
+++ apps/api/src/routes/v1/graphql/resolvers.ts
@@ -4,26 +4,26 @@ import { geTransform, proxyRestApi } from "./lib";
 
 export const resolvers: ApolloServerOptions<BaseContext>["resolvers"] = {
   Query: {
-    calendar: proxyRestApi("v1/rest/calendar"),
-    course: proxyRestApi("v1/rest/courses", { pathArg: "courseId" }),
-    courses: proxyRestApi("v1/rest/courses", { argsTransform: geTransform }),
-    allCourses: proxyRestApi("v1/rest/courses/all"),
-    rawGrades: proxyRestApi("v1/rest/grades/raw"),
-    aggregateGrades: proxyRestApi("v1/rest/grades/aggregate"),
-    gradesOptions: proxyRestApi("v1/rest/grades/options"),
-    aggregateByCourse: proxyRestApi("v1/rest/grades/aggregateByCourse", {
+    calendar: proxyRestApi("/v1/rest/calendar"),
+    course: proxyRestApi("/v1/rest/courses", { pathArg: "courseId" }),
+    courses: proxyRestApi("/v1/rest/courses", { argsTransform: geTransform }),
+    allCourses: proxyRestApi("/v1/rest/courses/all"),
+    rawGrades: proxyRestApi("/v1/rest/grades/raw"),
+    aggregateGrades: proxyRestApi("/v1/rest/grades/aggregate"),
+    gradesOptions: proxyRestApi("/v1/rest/grades/options"),
+    aggregateByCourse: proxyRestApi("/v1/rest/grades/aggregateByCourse", {
       argsTransform: geTransform,
     }),
-    aggregateByOffering: proxyRestApi("v1/rest/grades/aggregateByOffering", {
+    aggregateByOffering: proxyRestApi("/v1/rest/grades/aggregateByOffering", {
       argsTransform: geTransform,
     }),
-    instructor: proxyRestApi("v1/rest/instructors", { pathArg: "ucinetid" }),
-    instructors: proxyRestApi("v1/rest/instructors"),
-    allInstructors: proxyRestApi("v1/rest/instructors/all"),
-    larc: proxyRestApi("v1/rest/larc"),
-    websoc: proxyRestApi("v1/rest/websoc", { argsTransform: geTransform }),
-    depts: proxyRestApi("v1/rest/websoc/depts"),
-    terms: proxyRestApi("v1/rest/websoc/terms"),
-    week: proxyRestApi("v1/rest/week"),
+    instructor: proxyRestApi("/v1/rest/instructors", { pathArg: "ucinetid" }),
+    instructors: proxyRestApi("/v1/rest/instructors"),
+    allInstructors: proxyRestApi("/v1/rest/instructors/all"),
+    larc: proxyRestApi("/v1/rest/larc"),
+    websoc: proxyRestApi("/v1/rest/websoc", { argsTransform: geTransform }),
+    depts: proxyRestApi("/v1/rest/websoc/depts"),
+    terms: proxyRestApi("/v1/rest/websoc/terms"),
+    week: proxyRestApi("/v1/rest/week"),
   },
 };
