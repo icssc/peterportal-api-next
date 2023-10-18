818721b24d0bc28df1b3ccfa08a813c3b5e6a111
Aponia <brianmv@uci.edu>
2023-10-18 10:38:15 -0700

    feat: ✨ courses endpoint uses helper

--- apps/api/src/routes/v1/rest/courses/+endpoint.ts
+++ apps/api/src/routes/v1/rest/courses/+endpoint.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@libs/db";
-import { createErrorResult, createOKResult } from "@libs/lambda";
-import type { APIGatewayProxyHandler } from "aws-lambda";
+import { createHandler } from "@libs/lambda";
 import { ZodError } from "zod";
 
 import { constructPrismaQuery, normalizeCourse } from "./lib";
@@ -8,39 +7,30 @@ import { QuerySchema } from "./schema";
 
 const prisma = new PrismaClient();
 
-export const GET: APIGatewayProxyHandler = async (event, context) => {
+async function onWarm() {
+  await prisma.$connect();
+}
+
+export const get = createHandler(async (event, context, res) => {
   const headers = event.headers;
   const query = event.queryStringParameters;
   const requestId = context.awsRequestId;
-
-  /**
-   * TODO: handle warmer requests.
-   */
-
-  // if (request.isWarmerRequest) {
-  //   try {
-  //     await prisma.$connect();
-  //     return createOKResult("Warmed", headers, requestId);
-  //   } catch (e) {
-  //     createErrorResult(500, e, requestId);
-  //   }
-  // }
 
   try {
     const parsedQuery = QuerySchema.parse(query);
     // The query object being empty shouldn't return all courses, since there's /courses/all for that.
     if (!Object.keys(parsedQuery).length) {
-      return createErrorResult(400, "Course number not provided", requestId);
+      return res.createErrorResult(400, "Course number not provided", requestId);
     }
 
     const courses = await prisma.course.findMany({ where: constructPrismaQuery(parsedQuery) });
 
-    return createOKResult(courses.map(normalizeCourse), headers, requestId);
+    return res.createOKResult(courses.map(normalizeCourse), headers, requestId);
   } catch (error) {
     if (error instanceof ZodError) {
       const messages = error.issues.map((issue) => issue.message);
-      return createErrorResult(400, messages.join("; "), requestId);
+      return res.createErrorResult(400, messages.join("; "), requestId);
     }
-    return createErrorResult(400, error, requestId);
+    return res.createErrorResult(400, error, requestId);
   }
-};
+}, onWarm);
