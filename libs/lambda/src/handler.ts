bf3107fd0e314a01850acb3a455fa96fcbba2fba
Aponia <brianmv@uci.edu>
2023-10-18 10:29:41 -0700

    feat: ✨ done with create-handler helper

--- libs/lambda/src/handler.ts
+++ libs/lambda/src/handler.ts
@@ -1,29 +1,73 @@
-import type { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
+import type { APIGatewayProxyEvent, Callback, Context, APIGatewayProxyResult } from "aws-lambda";
 
 import { warmingRequestBody } from "./request";
 import { createOKResult, createErrorResult } from "./response";
 
+/**
+ * `res` object like Express.js .
+ */
 export type ResponseHelpers = {
-  ok: typeof createOKResult;
-  error: typeof createErrorResult;
+  /**
+   * Create an OK response.
+   */
+  createOKResult: typeof createOKResult;
+
+  /**
+   * Create an error response.
+   */
+  createErrorResult: typeof createErrorResult;
+
+  /**
+   * Create an ok response and send it.
+   */
+  ok: <T>(
+    payload: T,
+    requestHeaders: Record<string, string | undefined>,
+    requestId: string,
+  ) => void;
+
+  /**
+   * Create an error response and send it.
+   */
+  error: (statusCode: number, e: unknown, requestId: string) => void;
 };
 
 export type ExtendedApiGatewayHandler = (
   event: APIGatewayProxyEvent,
   context: Context,
   res: ResponseHelpers,
-) => APIGatewayProxyResult | Promise<APIGatewayProxyResult>;
+) => void | APIGatewayProxyResult | Promise<APIGatewayProxyResult | void>;
 
-export function createHandler(handler: ExtendedApiGatewayHandler): ExtendedApiGatewayHandler {
-  return async function (event, context) {
-    const res: ResponseHelpers = {
-      ok: (payload, headers, requestId) => createOKResult(payload, headers, requestId),
-      error: (statusCode, e, requestId) => createErrorResult(statusCode, e, requestId),
-    };
+/**
+ * Override the type from aws-lambda because it's bad.
+ */
+export type APIGatewayProxyHandler = (
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Callback<APIGatewayProxyResult>,
+) => void | Promise<APIGatewayProxyResult | void>;
 
+/**
+ * Creates a handler for API Gateway.
+ *
+ * Handles warming requests and provides utilities for formatting responses.
+ */
+export function createHandler(handler: ExtendedApiGatewayHandler): APIGatewayProxyHandler {
+  return async function (event, context, callback) {
     if (event.body === JSON.stringify(warmingRequestBody)) {
-      return res.ok("Successfully warmed!", event.headers, context.awsRequestId);
+      return createOKResult("Successfully warmed!", event.headers, context.awsRequestId);
     }
+
+    const res: ResponseHelpers = {
+      ok: (payload, headers, requestId) => {
+        callback(undefined, createOKResult(payload, headers, requestId));
+      },
+      error: (statusCode, e, requestId) => {
+        callback(undefined, createErrorResult(statusCode, e, requestId));
+      },
+      createOKResult,
+      createErrorResult,
+    };
 
     return handler(event, context, res);
   };
