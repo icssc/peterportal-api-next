116442a239c47043bf6e8ff68e24426389ab54cc
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-11-26 17:42:23 -0800

    fix: 🐛 remove extraneous roles

--- libs/db/sql/access_control.sql
+++ libs/db/sql/access_control.sql
@@ -18,38 +18,6 @@ CREATE ROLE api_prod_user LOGIN;
 
 GRANT INSERT ON public."CalendarTerm" TO api_prod_user;
 
-CREATE ROLE api_grades_updater LOGIN;
-
-GRANT
-SELECT
-,
-  INSERT,
-UPDATE,
-DELETE ON public."GradesInstructor" TO api_grades_updater;
-
-GRANT
-SELECT
-,
-  INSERT,
-UPDATE,
-DELETE ON public."GradesSection" TO api_grades_updater;
-
-CREATE ROLE api_registrar_scraper LOGIN;
-
-GRANT
-SELECT
-,
-  INSERT,
-UPDATE,
-DELETE ON public."Course" TO api_registrar_scraper;
-
-GRANT
-SELECT
-,
-  INSERT,
-UPDATE,
-DELETE ON public."Instructor" TO api_registrar_scraper;
-
 CREATE ROLE api_websoc_scraper LOGIN;
 
 GRANT
