475f6df13e77380e771771cee36c96b99685e22a
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-12-09 18:04:41 -0800

    fix: 🐛 use correct query engine filename

--- apps/api/bronya.config.ts
+++ apps/api/bronya.config.ts
@@ -82,7 +82,7 @@ const prismaSchema = resolve(libsDbDirectory, "prisma", prismaSchemaFile);
 /**
  * Name of the Prisma query engine file that's used on AWS Lambda.
  */
-const prismaQueryEngineFile = "libquery_engine-linux-rhel-openssl-1.0.x.so.node";
+const prismaQueryEngineFile = "libquery_engine-rhel-openssl-1.0.x.so.node";
 
 /**
  * Namespace for virtual files.
