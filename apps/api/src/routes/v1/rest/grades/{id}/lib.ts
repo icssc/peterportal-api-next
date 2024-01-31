ae7f9aa9cb867fecf8cbdf1638a6c8dcc908aea5
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-01-31 11:49:24 -0800

    fix: 🐛 grades queries for department codes with spaces

--- apps/api/src/routes/v1/rest/grades/{id}/lib.ts
+++ apps/api/src/routes/v1/rest/grades/{id}/lib.ts
@@ -145,7 +145,7 @@ export function constructPrismaQuery(parsedQuery: Query): Prisma.GradesSectionWh
     year,
     quarter,
     instructors: { some: { name: { contains: instructor, mode: "insensitive" } } },
-    department: department?.toUpperCase(),
+    department: department?.replace(/ /g, "").toUpperCase(),
     courseNumber: courseNumber?.toUpperCase(),
     courseNumeric,
     sectionCode,
