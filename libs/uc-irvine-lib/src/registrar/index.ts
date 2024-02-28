011e6d33df53125320c1ee9a6a17bb665ae6f831
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-02-28 13:08:47 -0800

    feat: ✨ add calendar scraper and dump endpoint (#132)

--- libs/uc-irvine-lib/src/registrar/index.ts
+++ libs/uc-irvine-lib/src/registrar/index.ts
@@ -1,76 +1,53 @@
 import type { QuarterDates } from "@peterportal-api/types";
-import { quarters } from "@peterportal-api/types";
 import { load } from "cheerio";
 import fetch from "cross-fetch";
 
+type ScrapedQuarterDates = Omit<QuarterDates, "year" | "quarter">;
+
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const terms = ["Fall", "Winter", "Spring", "Summer1", "Summer10wk", "Summer2"];
 
-function addSingleDateRow(
-  data: string[][],
-  index: number,
-  key: string,
-  record: Record<string, Partial<QuarterDates & { [p: string]: Date }>>,
-  year: string,
-  offset = 0,
-): void {
-  for (const [idx, date] of data[index].entries()) {
-    const currYear = idx == offset ? parseInt(year) : parseInt(year) + 1;
-    const [month, day] = date.split(" ");
-    record[`${currYear} ${quarters[idx + offset]}`][key] = new Date(
-      currYear,
-      months.indexOf(month),
-      parseInt(day),
-    );
-  }
-}
+const SOC_AVAIL = /schedule of classes available/i;
+const INST_START = /instruction begins/i;
+const INST_END = /instruction ends/i;
+const FINALS = /final examinations/i;
+const HYPHEN = /[-–]/;
 
-function addMultipleDateRow(
-  data: string[][],
-  index: number,
-  keyStart: string,
-  keyEnd: string,
-  record: Record<string, Partial<QuarterDates & { [p: string]: Date }>>,
-  year: string,
-  offset = 0,
-): void {
-  for (const [idx, date] of data[index].entries()) {
-    const currYear = idx == offset ? parseInt(year) : parseInt(year) + 1;
+const parseDate = (year: number, dateString: string) =>
+  new Date(
+    year,
+    months.indexOf(dateString.split(" ")[0]),
+    Number.parseInt(dateString.split(" ")[1], 10),
+  );
 
-    const start = date.split("–")[0];
-
-    let end = date.split("–")[1];
-
-    if (end === undefined) {
-      end = start;
-    }
-
-    const [startMonth, startDay] = start.split(" ");
-
-    let [endMonth, endDay] = end.split(" ");
-
-    if (endDay === undefined) {
-      endDay = endMonth;
-      endMonth = startMonth;
-    }
-
-    record[`${currYear} ${quarters[idx + offset]}`][keyStart] = new Date(
-      currYear,
-      months.indexOf(startMonth),
-      parseInt(startDay),
-    );
-
-    record[`${currYear} ${quarters[idx + offset]}`][keyEnd] = new Date(
-      currYear,
-      months.indexOf(endMonth),
-      parseInt(endDay),
-    );
-  }
-}
-
+const parseDateRange = (year: number, dateRangeString: string): [Date, Date] => [
+  new Date(
+    year,
+    months.indexOf(dateRangeString.split(" ")[0]),
+    Number.parseInt(dateRangeString.split(" ")[1].split(HYPHEN)[0], 10),
+  ),
+  dateRangeString.split(HYPHEN)[1]?.match(/[A-Za-z]/)
+    ? new Date(
+        year,
+        months.indexOf(dateRangeString.split(HYPHEN)[1].split(" ")[0]),
+        Number.parseInt(dateRangeString.split(HYPHEN)[1].split(" ")[1], 10),
+      )
+    : new Date(
+        year,
+        months.indexOf(dateRangeString.split(" ")[0]),
+        Number.parseInt(
+          dateRangeString.split(" ")[1].split(HYPHEN)[1] ??
+            dateRangeString.split(" ")[1].split(HYPHEN)[0],
+          10,
+        ),
+      ),
+];
 /**
  * Returns relevant date data for each term in the given academic year.
  */
-export async function getTermDateData(year: string): Promise<Record<string, QuarterDates>> {
+export async function getTermDateData(year: string): Promise<Record<string, ScrapedQuarterDates>> {
+  const yearNum = Number.parseInt(year, 10);
+
   if (year.length !== 4 || isNaN(parseInt(year))) {
     throw new Error("Error: Invalid year provided.");
   }
@@ -79,105 +56,68 @@ export async function getTermDateData(year: string): Promise<Record<string, Quar
 
   const response = await fetch(
     `https://www.reg.uci.edu/calendars/quarterly/${year}-${
-      parseInt(year, 10) + 1
-    }/quarterly${shortYear}-${parseInt(shortYear, 10) + 1}.html`,
+      Number.parseInt(year, 10) + 1
+    }/quarterly${shortYear}-${Number.parseInt(shortYear, 10) + 1}.html`,
   );
 
   if (response.status === 404) {
     return {};
   }
 
-  const quarterData: string[][] = [];
+  const data = load(await response.text())("table.calendartable")
+    .text()
+    .split("\n")
+    .map((x) => x.trim())
+    .filter((x) => x.length);
 
-  const summerSessionData: string[][] = [];
+  const socIdx = data.findIndex((x) => x.match(SOC_AVAIL)) + 1;
+  const socSummerIdx = data.findLastIndex((x) => x.match(SOC_AVAIL)) + 1;
+  const instStartIdx = data.findIndex((x) => x.match(INST_START)) + 1;
+  const instStartSummerIdx = data.findLastIndex((x) => x.match(INST_START)) + 1;
+  const instEndIdx = data.findIndex((x) => x.match(INST_END)) + 1;
+  const instEndSummerIdx = data.findLastIndex((x) => x.match(INST_END)) + 1;
+  const finalsIdx = data.findIndex((x) => x.match(FINALS)) + 1;
+  const finalsSummerIdx = data.findLastIndex((x) => x.match(FINALS)) + 1;
 
-  const $ = load(await response.text());
+  const term = (i: number) => `${yearNum + Number(i > 0)} ${terms[i]}`;
 
-  const $table = $("table.calendartable");
-
-  $table
-    .eq(2)
-    .find("tr")
-    .each(function () {
-      quarterData.push(
-        $(this)
-          .text()
-          .split("\n")
-          .map((x) => x.trim())
-          .filter((x) => x.length)
-          .slice(1),
-      );
-    });
-
-  $table
-    .eq(4)
-    .find("tr")
-    .each(function () {
-      summerSessionData.push(
-        $(this)
-          .text()
-          .split("\n")
-          .map((x) => x.trim())
-          .filter((x) => x.length)
-          .slice(1),
-      );
-    });
-
-  const ret = quarters
-    .map((x, i) => `${i == 0 ? year : parseInt(year) + 1} ${x}`)
-    .reduce(
-      (p, c) => {
-        p[c] = {};
-        return p;
-      },
-      {} as Record<string, Partial<QuarterDates>>,
-    );
-
-  addSingleDateRow(quarterData, 2, "instructionStart", ret, year);
-  // After the 2022-23 academic year, the dates for the end of instruction and the final examinations period was shifted up by one row.
-  // This change accounts for the difference in table row numbering caused by this change.
-  addSingleDateRow(
-    quarterData,
-    17 - Number(Number.parseInt(year, 10) > 2022),
-    "instructionEnd",
-    ret,
-    year,
+  const soc: Record<string, Date> = Object.fromEntries(
+    [...data.slice(socIdx, socIdx + 3), ...data.slice(socSummerIdx, socSummerIdx + 3)].map(
+      (x, i) => [term(i), parseDate(yearNum + Number(i > 1), x)],
+    ),
   );
-  addMultipleDateRow(
-    quarterData,
-    18 - Number(Number.parseInt(year, 10) > 2022),
-    "finalsStart",
-    "finalsEnd",
-    ret,
-    year,
+  const instStart: Record<string, Date> = Object.fromEntries(
+    [
+      ...data.slice(instStartIdx, instStartIdx + 3),
+      ...data.slice(instStartSummerIdx, instStartSummerIdx + 3),
+    ].map((x, i) => [term(i), parseDate(yearNum + Number(i > 0), x)]),
   );
-  addSingleDateRow(
-    summerSessionData,
-    // Before the 2021-22 academic year, Juneteenth was either not observed or observed during one of the Summer Sessions.
-    // This change accounts for the difference in table row numbering caused by this change.
-    2 + Number(parseInt(year, 10) > 2020),
-    "instructionStart",
-    ret,
-    year,
-    3,
+  const instEnd: Record<string, Date> = Object.fromEntries(
+    [
+      ...data.slice(instEndIdx, instEndIdx + 3),
+      ...data.slice(instEndSummerIdx, instEndSummerIdx + 3),
+    ].map((x, i) => [term(i), parseDate(yearNum + Number(i > 0), x)]),
   );
-  addSingleDateRow(summerSessionData, 6, "instructionEnd", ret, year, 3);
-  addMultipleDateRow(summerSessionData, 7, "finalsStart", "finalsEnd", ret, year, 3);
+  const finals: Record<string, [Date, Date]> = Object.fromEntries(
+    [
+      ...data.slice(finalsIdx, finalsIdx + 3),
+      ...data.slice(finalsSummerIdx, finalsSummerIdx + 3),
+    ].map((x, i) => [term(i), parseDateRange(yearNum + Number(i > 0), x)]),
+  );
 
-  // Normalize all terms to start on a Monday, or a Thursday if it is Fall.
-  for (const key in ret) {
-    if (key.includes("Fall")) {
-      (ret[key] as QuarterDates).instructionStart.setDate(
-        (ret[key] as QuarterDates).instructionStart.getDate() -
-          ((ret[key] as QuarterDates).instructionStart.getDay() - 4),
-      );
-    } else {
-      (ret[key] as QuarterDates).instructionStart.setDate(
-        (ret[key] as QuarterDates).instructionStart.getDate() -
-          ((ret[key] as QuarterDates).instructionStart.getDay() - 1),
-      );
-    }
-  }
-
-  return ret as Record<string, QuarterDates>;
+  return Object.fromEntries(
+    Array(6)
+      .fill(0)
+      .map((_, i) => term(i))
+      .map((x) => [
+        x,
+        {
+          socAvailable: soc[x],
+          instructionStart: instStart[x],
+          instructionEnd: instEnd[x],
+          finalsStart: finals[x][0],
+          finalsEnd: finals[x][1],
+        },
+      ]),
+  );
 }
---
+++ services/calendar-scraper/build.mjs
@@ -0,0 +1,72 @@
+import { chmod, copyFile, mkdir, rm } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { build } from "esbuild";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * @see https://github.com/evanw/esbuild/issues/1921#issuecomment-1623640043
+ */
+// language=JavaScript
+const js = `
+  import topLevelModule from "node:module";
+  import topLevelUrl from "node:url";
+  import topLevelPath from "node:path";
+
+  const require = topLevelModule.createRequire(import.meta.url);
+  const __filename = topLevelUrl.fileURLToPath(import.meta.url);
+  const __dirname = topLevelPath.dirname(__filename);
+`;
+
+async function buildApp() {
+  const options = {
+    entryPoints: { index: "index.ts" },
+    outdir: "dist",
+    outExtension: { ".js": ".mjs" },
+    bundle: true,
+    minify: true,
+    format: "esm",
+    platform: "node",
+    target: "node20",
+    logLevel: "info",
+    banner: { js },
+    plugins: [
+      {
+        name: "clean",
+        setup(build) {
+          build.onStart(async () => {
+            await rm(join(__dirname, "dist/"), { recursive: true, force: true });
+            await mkdir(join(__dirname, "dist/"));
+          });
+        },
+      },
+      {
+        name: "copy",
+        setup(build) {
+          build.onEnd(async () => {
+            await copyFile(
+              join(
+                __dirname,
+                "../../libs/db/node_modules/prisma/libquery_engine-linux-arm64-openssl-3.0.x.so.node",
+              ),
+              join(__dirname, "dist/libquery_engine-linux-arm64-openssl-3.0.x.so.node"),
+            );
+            await copyFile(
+              join(__dirname, "../../libs/db/prisma/schema.prisma"),
+              join(__dirname, "dist/schema.prisma"),
+            );
+            await chmod(
+              join(__dirname, "dist/libquery_engine-linux-arm64-openssl-3.0.x.so.node"),
+              0o755,
+            );
+          });
+        },
+      },
+    ],
+  };
+  await build(options);
+}
+
+buildApp().then();
