28fd0a7fb5f7a836e5028771f11b8e9d60ed2291
Andrew Wang <awwang3@uci.edu>
2024-05-15 18:17:10 -0700

    fix: 🐛 return parsed departments & terms

--- services/departments-terms-scraper/src/index.ts
+++ services/departments-terms-scraper/src/index.ts
@@ -1,13 +1,19 @@
 import { CheerioAPI, load } from "cheerio";
 import fetch from "cross-fetch";
 
-async function fetchWebSoc() {
-  const response = await fetch("https://www.reg.uci.edu/perl/WebSoc");
-  const body = await response.text();
-  return load(body);
+function formatDepartment(department: string) {
+  const match = department.match(/^(?<code>.+?) \.(?:\.|\s)+(?<name>.+)$/);
+  if (match?.groups) {
+    return {
+      code: match.groups.code,
+      name: match.groups.name,
+    };
+  }
+
+  throw new Error(`Failed to parse department: ${department}`);
 }
 
-async function getDepartments(webSocContent: CheerioAPI): Promise<string[]> {
+async function getDepartments(webSocContent: CheerioAPI) {
   const $ = webSocContent;
 
   const departments: string[] = [];
@@ -19,10 +25,23 @@ async function getDepartments(webSocContent: CheerioAPI): Promise<string[]> {
     }
   });
 
-  return departments;
+  return departments.map(formatDepartment);
 }
 
-async function getTerms(webSocContent: CheerioAPI): Promise<string[]> {
+function formatTerm(term: string) {
+  const match = term.match(/^(?<year>\d+) {2}(?<term>.+)$/);
+
+  if (match?.groups) {
+    return {
+      year: match.groups.year,
+      term: match.groups.term,
+    };
+  }
+
+  throw new Error(`Failed to parse term: ${term}`);
+}
+
+async function getTerms(webSocContent: CheerioAPI) {
   const $ = webSocContent;
 
   const terms: string[] = [];
@@ -31,7 +50,13 @@ async function getTerms(webSocContent: CheerioAPI): Promise<string[]> {
     terms.push(termText);
   });
 
-  return terms;
+  return terms.map(formatTerm);
+}
+
+async function fetchWebSoc() {
+  const response = await fetch("https://www.reg.uci.edu/perl/WebSoc");
+  const body = await response.text();
+  return load(body);
 }
 
 async function getDepartmentsTerms() {
