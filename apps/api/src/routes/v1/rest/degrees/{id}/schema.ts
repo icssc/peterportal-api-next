01f5751cd4ed49f0e553e51d57833e206282c5cf
Aponia <brianmv@uci.edu>
2024-05-19 12:56:58 -0700

    refactor(api): ♻️ degrees endpoint (#147)

--- apps/api/src/routes/v1/rest/degrees/{id}/schema.ts
+++ apps/api/src/routes/v1/rest/degrees/{id}/schema.ts
@@ -1,14 +1,42 @@
 import { z } from "zod";
 
-export const ProgramSchema = z.union([
-  z.object({ id: z.string() }),
-  z.object({ degreeId: z.string().optional(), nameContains: z.string().optional() }),
-  z.object({}),
-]);
+export const ProgramSchema = z
+  .union([
+    z.object({ id: z.string() }),
+    z.object({ degreeId: z.string().optional(), nameContains: z.string().optional() }),
+    z.object({}),
+  ])
+  .transform((data) => {
+    if ("id" in data) {
+      return { type: "id" as const, ...data };
+    }
 
-export const SpecializationSchema = z.union([
-  z.object({ id: z.string() }),
-  z.object({ majorId: z.string() }),
-  z.object({ nameContains: z.string() }),
-  z.object({}),
-]);
+    if ("degreeId" in data && data.degreeId != null) {
+      return { type: "degreeOrName" as const, degreeId: data.degreeId, ...data };
+    }
+
+    return { type: "empty" as const, ...data };
+  });
+
+export const SpecializationSchema = z
+  .union([
+    z.object({ id: z.string() }),
+    z.object({ majorId: z.string() }),
+    z.object({ nameContains: z.string() }),
+    z.object({}),
+  ])
+  .transform((data) => {
+    if ("id" in data) {
+      return { type: "id" as const, ...data };
+    }
+
+    if ("majorId" in data) {
+      return { type: "major" as const, ...data };
+    }
+
+    if ("nameContains" in data) {
+      return { type: "name" as const, ...data };
+    }
+
+    return { type: "empty" as const, ...data };
+  });
