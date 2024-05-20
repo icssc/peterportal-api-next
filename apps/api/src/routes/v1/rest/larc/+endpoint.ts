7d5b44b51c945319159805073e289147e4b2f78d
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-05-20 13:24:05 -0700

    feat: ✨ implement larc scraper, change route to get from cache

--- apps/api/src/routes/v1/rest/larc/+endpoint.ts
+++ apps/api/src/routes/v1/rest/larc/+endpoint.ts
@@ -1,10 +1,10 @@
+import { PrismaClient } from "@libs/db";
 import { createHandler } from "@libs/lambda";
-import { load } from "cheerio";
-import { fetch } from "cross-fetch";
 import { ZodError } from "zod";
 
-import { fmtBldg, fmtDays, fmtTime, quarterToLarcSuffix } from "./lib";
 import { QuerySchema } from "./schema";
+
+const prisma = new PrismaClient();
 
 export const GET = createHandler(async (event, context, res) => {
   const headers = event.headers;
@@ -14,55 +14,11 @@ export const GET = createHandler(async (event, context, res) => {
   try {
     const { year, quarter } = QuerySchema.parse(query);
 
-    // SS10wk does not have LARC sessions apparently
-    if (quarter === "Summer10wk") return res.createOKResult([], headers, requestId);
-
-    // TODO: move this code to its own scraper, and rewrite this route to fetch
-    // data from the DB.
-
-    const html = await fetch(
-      `https://enroll.larc.uci.edu/${year}${quarterToLarcSuffix(quarter)}`,
-    ).then((response) => response.text());
-
-    const $ = load(html);
-
-    const larcSections = $(".tutorial-group")
-      .toArray()
-      .map((card) => {
-        const match = $(card)
-          .find(".card-header")
-          .text()
-          .trim()
-          .match(
-            /(?<courseNumber>[^()]*)( \(same as (?<sameAs>.*)\))? - (.*) \((?<courseName>.*)\)/,
-          );
-
-        const sections = $(card)
-          .find(".list-group")
-          .toArray()
-          .map((group) => {
-            const rows = $(group).find(".col-lg-4");
-
-            const [days, time] = $(rows[0])
-              .find(".col")
-              .map((_, col) => $(col).text().trim());
-
-            const [instructor, building] = $(rows[1])
-              .find(".col")
-              .map((_, col) => $(col).text().trim());
-
-            return {
-              days: fmtDays(days),
-              time: fmtTime(time),
-              instructor,
-              bldg: fmtBldg(building),
-            };
-          });
-
-        return { courseInfo: { ...match?.groups }, sections };
-      });
-
-    return res.createOKResult(larcSections, headers, requestId);
+    return res.createOKResult(
+      (await prisma.larcTerm.findFirst({ where: { year, quarter } }))?.courses ?? [],
+      headers,
+      requestId,
+    );
   } catch (e) {
     if (e instanceof ZodError) {
       const messages = e.issues.map((issue) => issue.message);
